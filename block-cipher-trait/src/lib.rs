cc9247ff979d9f04db7fe0362378e14aef1b7201
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-09-02 23:39:55 +0300

    Updated block cipher trait to in-place operations

--- block-cipher-trait/src/lib.rs
+++ block-cipher-trait/src/lib.rs
@@ -10,47 +10,31 @@ pub type Block<BlockSize> = GenericArray<u8, BlockSize>;
 pub trait BlockCipher {
     type BlockSize: ArrayLength<u8>;
 
-    fn encrypt_block(&self, input: &Block<Self::BlockSize>,
-                     output: &mut Block<Self::BlockSize>);
+    fn encrypt_block(&self, block: &mut Block<Self::BlockSize>);
 
-    fn decrypt_block(&self, input: &Block<Self::BlockSize>,
-                     output: &mut Block<Self::BlockSize>);
+    fn decrypt_block(&self, block: &mut Block<Self::BlockSize>);
 }
 
-pub trait BlockCipherFixKey:BlockCipher {
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct InvalidKeyLength;
+
+pub trait BlockCipherFixKey: BlockCipher {
     type KeySize: ArrayLength<u8>;
 
     fn new(key: &GenericArray<u8, Self::KeySize>) -> Self;
 }
 
-pub trait BlockCipherVarKey:BlockCipher {
-    fn new(key: &[u8]) -> Self;
-}
-
-
-/// Temporary here before it's merged into generic-array
-#[inline]
-pub fn from_slice<T, N: ArrayLength<T>>(slice: &[T]) -> &GenericArray<T, N> {
-    assert_eq!(slice.len(), N::to_usize());
-    unsafe {
-        &*(slice.as_ptr() as *const GenericArray<T, N>)
-    }
+pub trait BlockCipherVarKey: BlockCipher + Sized {
+    fn new(key: &[u8]) -> Result<Self, InvalidKeyLength>;
 }
 
 impl<B: BlockCipherFixKey> BlockCipherVarKey for B {
-    fn new(key: &[u8]) -> Self {
-        assert_eq!(key.len(), B::KeySize::to_usize());
-        B::new(from_slice(key))
+    fn new(key: &[u8]) -> Result<Self, InvalidKeyLength> {
+        if key.len() != B::KeySize::to_usize() {
+            Err(InvalidKeyLength)
+        } else {
+            Ok(B::new(GenericArray::from_slice(key)))
+        }
     }
 }
-
-/*
-pub trait BlockEncryptorX8 {
-    fn block_size(&self) -> usize;
-    fn encrypt_block_x8(&self, input: &[u8], output: &mut [u8]);
-}
-
-pub trait BlockDecryptorX8 {
-    fn block_size(&self) -> usize;
-    fn decrypt_block_x8(&self, input: &[u8], output: &mut [u8]);
-}*/
