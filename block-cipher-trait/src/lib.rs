4a781e5299b1d811a366aa08a36ccb42751cee0d
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-09-06 00:37:47 +0300

    block-cipher-trait v0.4.0

--- block-cipher-trait/src/lib.rs
+++ block-cipher-trait/src/lib.rs
@@ -1,40 +1,52 @@
-//! This crate defines a simple trait used to define block ciphers
+//! This crate defines a set of simple traits used to define functionality of
+//! block ciphers.
 #![no_std]
 extern crate generic_array;
 
 use generic_array::{GenericArray, ArrayLength};
 use generic_array::typenum::Unsigned;
 
-pub type Block<BlockSize> = GenericArray<u8, BlockSize>;
+type Block<BlockSize> = GenericArray<u8, BlockSize>;
 
+/// Main block cipher trait which defines in-place encryption and decryption
+/// over single block
 pub trait BlockCipher {
     type BlockSize: ArrayLength<u8>;
 
+    /// Encrypt block in-place
     fn encrypt_block(&self, block: &mut Block<Self::BlockSize>);
 
+    /// Decrypt block in-place
     fn decrypt_block(&self, block: &mut Block<Self::BlockSize>);
 }
 
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct InvalidKeyLength;
-
-pub trait BlockCipherFixKey: BlockCipher {
+/// Trait for creation of block cipher with fixed size key
+pub trait NewFixKey: BlockCipher {
     type KeySize: ArrayLength<u8>;
 
+    /// Create new block cipher instance with given fixed size key
     fn new(key: &GenericArray<u8, Self::KeySize>) -> Self;
 }
 
-pub trait BlockCipherVarKey: BlockCipher + Sized {
+/// Error struct which used with `NewVarKey`
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct InvalidKeyLength;
+
+/// Trait for creation of block cipher with variable size keys.
+///
+/// This trait is auto implemented for `NewFixKey`.
+pub trait NewVarKey: BlockCipher + Sized {
+    /// Create new block cipher instance with given key, if length of given
+    /// key is unsupported by implementation error will be returned.
     fn new(key: &[u8]) -> Result<Self, InvalidKeyLength>;
 }
 
-impl<B: BlockCipherFixKey> BlockCipherVarKey for B {
+impl<T: NewFixKey> NewVarKey for T {
     fn new(key: &[u8]) -> Result<Self, InvalidKeyLength> {
-        if key.len() != B::KeySize::to_usize() {
+        if key.len() != T::KeySize::to_usize() {
             Err(InvalidKeyLength)
         } else {
-            Ok(B::new(GenericArray::from_slice(key)))
+            Ok(T::new(GenericArray::from_slice(key)))
         }
     }
 }
