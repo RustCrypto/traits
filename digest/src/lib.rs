4e4ecaf0c951995525570ed1212d43fe56c2ae7a
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-10-30 05:12:38 +0300

    generic-buffer update to v0.9, added impl_opaque_debug macro, generic_array re-export

--- digest/src/lib.rs
+++ digest/src/lib.rs
@@ -5,7 +5,7 @@
 //! hashing `Read`ers) To enable it turn on `std` feature in your `Cargo.toml`
 //! for this crate.
 #![cfg_attr(not(feature = "std"), no_std)]
-extern crate generic_array;
+pub extern crate generic_array;
 
 #[cfg(feature = "std")]
 use std as core;
@@ -74,4 +74,21 @@ pub trait ExtendableOutput {
 
     /// Finalize hash function and return XOF reader
     fn xof_result(self) -> Self::Reader;
+}
+
+/// Macro for defining opaque `Debug` implementation. It will use the following
+/// format: "HasherName { ... }". While it's convinient to have it
+/// (e.g. for including in other structs), it could be undesirable to leak
+/// internall state, which can happen for example through uncareful logging.
+#[macro_export]
+macro_rules! impl_opaque_debug {
+    ($state:ty) => {
+        use core::fmt;
+
+        impl fmt::Debug for $state {
+            fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+                write!(f, concat!(stringify!($state), " {{ ... }}"))
+            }
+        }
+    }
 }
