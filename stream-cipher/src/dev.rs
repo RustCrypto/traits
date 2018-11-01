bc79ccb48b97aa1c7ea1b442763be126900094ae
newpavlov <newpavlov@gmail.com>
2018-11-01 16:40:29 +0300

    stream-cipher v0.3.0

--- stream-cipher/src/dev.rs
+++ stream-cipher/src/dev.rs
@@ -1,12 +1,11 @@
-/// Test core functionality of stream cipher, i.e
-/// `StreamCipherCore::apply_keystream` method
+/// Test core functionality of synchronous stream cipher
 #[macro_export]
-macro_rules! new_core_test {
+macro_rules! new_sync_test {
     ($name:ident, $cipher:ty, $test_name:expr) => {
         #[test]
         fn $name() {
             use stream_cipher::generic_array::GenericArray;
-            use stream_cipher::{StreamCipherCore, NewFixStreamCipher};
+            use stream_cipher::{SyncStreamCipher, NewStreamCipher};
             use stream_cipher::blobby::Blob4Iterator;
 
             let data = include_bytes!(concat!("data/", $test_name, ".blb"));
@@ -37,8 +36,7 @@ macro_rules! new_core_test {
     }
 }
 
-/// Test stream cipher seeking capabilities, i.e. `StreamCipherSeek::seek`
-/// method
+/// Test stream synchronous stream cipher seeking capabilities
 #[macro_export]
 macro_rules! new_seek_test {
     ($name:ident, $cipher:ty, $test_name:expr) => {
@@ -46,7 +44,7 @@ macro_rules! new_seek_test {
         fn $name() {
             use stream_cipher::generic_array::GenericArray;
             use stream_cipher::{
-                StreamCipherCore, StreamCipherSeek, NewFixStreamCipher
+                SyncStreamCipher, SyncStreamCipherSeek, NewStreamCipher
             };
             use stream_cipher::blobby::Blob4Iterator;
 
@@ -77,16 +75,73 @@ macro_rules! new_seek_test {
     }
 }
 
-/// Create benchmarks for NewFixStreamCipher implementers
+/// Test core functionality of asynchronous stream cipher
 #[macro_export]
-macro_rules! bench_fixed {
+macro_rules! new_async_test {
+    ($name:ident, $test_name:expr, $cipher:ty) => {
+        #[test]
+        fn $name() {
+            use stream_cipher::generic_array::GenericArray;
+            use stream_cipher::{StreamCipher, NewStreamCipher};
+            use stream_cipher::blobby::Blob4Iterator;
+
+            fn run_test(key: &[u8], iv: &[u8], plaintext: &[u8], ciphertext: &[u8])
+                -> Option<&'static str>
+            {
+                for n in 1..=plaintext.len() {
+                    let mut mode = <$cipher>::new_var(key, iv).unwrap();
+                    let mut buf = plaintext.to_vec();
+                    for chunk in buf.chunks_mut(n) {
+                        mode.encrypt(chunk);
+                    }
+                    if buf != &ciphertext[..] { return Some("encrypt"); }
+                }
+
+                for n in 1..=plaintext.len() {
+                    let mut mode = <$cipher>::new_var(key, iv).unwrap();
+                    let mut buf = ciphertext.to_vec();
+                    for chunk in buf.chunks_mut(n) {
+                        mode.decrypt(chunk);
+                    }
+                    if buf != &plaintext[..] { return Some("decrypt"); }
+                }
+
+                None
+            }
+
+            let data = include_bytes!(concat!("data/", $test_name, ".blb"));
+
+            for (i, row) in Blob4Iterator::new(data).unwrap().enumerate() {
+                let key = row[0];
+                let iv = row[1];
+                let plaintext = row[2];
+                let ciphertext = row[3];
+                if let Some(desc) = run_test(key, iv, plaintext, ciphertext) {
+                    panic!("\n\
+                        Failed test №{}: {}\n\
+                        key:\t{:?}\n\
+                        iv:\t{:?}\n\
+                        plaintext:\t{:?}\n\
+                        ciphertext:\t{:?}\n",
+                        i, desc, key, iv, plaintext, ciphertext,
+                    );
+                }
+            }
+
+        }
+    }
+}
+
+/// Create synchronous stream cipher benchmarks
+#[macro_export]
+macro_rules! bench_sync {
     ($name:ident, $cipher:path, $data_len:expr) => {
         #[bench]
         pub fn $name(bh: &mut Bencher) {
             let key = Default::default();
             let nonce = Default::default();
             let mut cipher = <$cipher>::new(&key, &nonce);
-            let mut data = [77; $data_len];
+            let mut data = get_data($data_len);
 
             bh.iter(|| {
                 cipher.apply_keystream(&mut data);
@@ -100,12 +155,69 @@ macro_rules! bench_fixed {
 
         use test::Bencher;
         use stream_cipher::generic_array::GenericArray;
-        use stream_cipher::{StreamCipherCore, NewFixStreamCipher};
+        use stream_cipher::{SyncStreamCipher, NewStreamCipher};
 
-        bench_fixed!(bench1_10,     $cipher, 10);
-        bench_fixed!(bench2_100,    $cipher, 100);
-        bench_fixed!(bench3_1000,   $cipher, 1000);
-        bench_fixed!(bench4_10000 , $cipher, 10000);
-        bench_fixed!(bench5_100000, $cipher, 100000);
+        #[inline(never)]
+        fn get_data(n: usize) -> Vec<u8> {
+            vec![77; n]
+        }
+
+        bench_sync!(bench1_10,     $cipher, 10);
+        bench_sync!(bench2_100,    $cipher, 100);
+        bench_sync!(bench3_1000,   $cipher, 1000);
+        bench_sync!(bench4_10000,  $cipher, 10000);
+        bench_sync!(bench5_100000, $cipher, 100000);
+    }
+}
+
+/// Create synchronous stream cipher benchmarks
+#[macro_export]
+macro_rules! bench_async {
+    ($enc_name:ident, $dec_name:ident, $cipher:path, $data_len:expr) => {
+        #[bench]
+        pub fn $enc_name(bh: &mut Bencher) {
+            let key = Default::default();
+            let nonce = Default::default();
+            let mut cipher = <$cipher>::new(&key, &nonce);
+            let mut data = get_data($data_len);
+
+            bh.iter(|| {
+                cipher.encrypt(&mut data);
+                test::black_box(&data);
+            });
+            bh.bytes = data.len() as u64;
+        }
+
+        #[bench]
+        pub fn $dec_name(bh: &mut Bencher) {
+            let key = Default::default();
+            let nonce = Default::default();
+            let mut cipher = <$cipher>::new(&key, &nonce);
+            let mut data = get_data($data_len);
+
+            bh.iter(|| {
+                cipher.decrypt(&mut data);
+                test::black_box(&data);
+            });
+            bh.bytes = data.len() as u64;
+        }
+    };
+    ($cipher:path) => {
+        extern crate test;
+
+        use test::Bencher;
+        use stream_cipher::generic_array::GenericArray;
+        use stream_cipher::{StreamCipher, NewStreamCipher};
+
+        #[inline(never)]
+        fn get_data(n: usize) -> Vec<u8> {
+            vec![77; n]
+        }
+
+        bench_async!(encrypt_10,     decrypt_10,     $cipher, 10);
+        bench_async!(encrypt_100,    decrypt_100,    $cipher, 100);
+        bench_async!(encrypt_1000,   decrypt_1000,   $cipher, 1000);
+        bench_async!(encrypt_10000,  decrypt_10000,  $cipher, 10000);
+        bench_async!(encrypt_100000, decrypt_100000, $cipher, 100000);
     }
 }
--- stream-cipher/src/lib.rs
+++ stream-cipher/src/lib.rs
@@ -21,41 +21,10 @@ mod errors;
 
 pub use errors::{LoopError, InvalidKeyNonceLength};
 
-/// Synchronous stream cipher core trait
-pub trait StreamCipherCore {
-    /// Apply keystream to the data.
-    ///
-    /// It will XOR generated keystream with the data, which can be both
-    /// encryption and decryption.
-    ///
-    /// # Panics
-    /// If end of the keystream will be reached with the given data length,
-    /// method will panic without modifying the provided `data`.
-    #[inline]
-    fn apply_keystream(&mut self, data: &mut [u8]) {
-        self.try_apply_keystream(data)
-            .expect("stream cipher loop detected")
-    }
-
-    /// Apply keystream to the data, but return an error if end of a keystream
-    /// will be reached.
-    ///
-    /// If end of the keystream will be achieved with the given data length,
-    /// method will return `Err(LoopError)` without modifying provided `data`.
-    fn try_apply_keystream(&mut self, data: &mut [u8]) -> Result<(), LoopError>;
-}
-
-/// Synchronous stream cipher seeking trait
-pub trait StreamCipherSeek {
-    /// Return current position of a keystream in bytes from the beginning.
-    fn current_pos(&self) -> u64;
-    /// Seek keystream to the given `pos` in bytes.
-    fn seek(&mut self, pos: u64);
-}
-
-//TODO: rename to NewStreamCipher in next minor release
-/// Synchronous stream cipher creation trait
-pub trait NewFixStreamCipher: Sized {
+/// Stream cipher creation trait.
+///
+/// It can be used for creation of synchronous and asynchronous ciphers.
+pub trait NewStreamCipher: Sized {
     /// Key size in bytes
     type KeySize: ArrayLength<u8>;
     /// Nonce size in bytes
@@ -68,6 +37,7 @@ pub trait NewFixStreamCipher: Sized {
     ) -> Self;
 
     /// Create new stream cipher instance from variable length key and nonce.
+    #[inline]
     fn new_var(key: &[u8], nonce: &[u8]) -> Result<Self, InvalidKeyNonceLength> {
         let kl = Self::KeySize::to_usize();
         let nl = Self::NonceSize::to_usize();
@@ -78,5 +48,65 @@ pub trait NewFixStreamCipher: Sized {
             let nonce = GenericArray::from_slice(nonce);
             Ok(Self::new(key, nonce))
         }
+    }
+}
+
+/// Synchronous stream cipher core trait.
+pub trait SyncStreamCipher {
+    /// Apply keystream to the data.
+    ///
+    /// It will XOR generated keystream with the data, which can be both
+    /// encryption and decryption.
+    ///
+    /// # Panics
+    /// If end of the keystream will be reached with the given data length,
+    /// method will panic without modifying the provided `data`.
+    #[inline]
+    fn apply_keystream(&mut self, data: &mut [u8]) {
+        let res = self.try_apply_keystream(data);
+        if res.is_err() {
+            panic!("stream cipher loop detected");
+        }
+    }
+
+    /// Apply keystream to the data, but return an error if end of a keystream
+    /// will be reached.
+    ///
+    /// If end of the keystream will be achieved with the given data length,
+    /// method will return `Err(LoopError)` without modifying provided `data`.
+    fn try_apply_keystream(&mut self, data: &mut [u8]) -> Result<(), LoopError>;
+}
+
+/// Synchronous stream cipher seeking trait.
+pub trait SyncStreamCipherSeek {
+    /// Return current position of a keystream in bytes from the beginning.
+    fn current_pos(&self) -> u64;
+
+    /// Seek keystream to the given `pos` in bytes.
+    fn seek(&mut self, pos: u64);
+}
+
+/// Stream cipher core trait which covers both synchronous and asynchronous
+/// ciphers.
+///
+/// Note that for synchronous ciphers `encrypt` and `decrypt` are equivalent to
+/// each other.
+pub trait StreamCipher {
+    /// Encrypt data in place.
+    fn encrypt(&mut self, data: &mut [u8]);
+
+    /// Decrypt data in place.
+    fn decrypt(&mut self, data: &mut [u8]);
+}
+
+impl<C: SyncStreamCipher> StreamCipher for C {
+    #[inline(always)]
+    fn encrypt(&mut self, data: &mut [u8]) {
+        SyncStreamCipher::apply_keystream(self, data);
+    }
+
+    #[inline(always)]
+    fn decrypt(&mut self, data: &mut [u8]) {
+        SyncStreamCipher::apply_keystream(self, data);
     }
 }
