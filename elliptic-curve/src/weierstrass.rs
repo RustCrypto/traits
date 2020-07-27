005e578369f5fdb2c003c35057b7c2454707d9be
Tony Arcieri <bascule@gmail.com>
2020-07-27 11:22:11 -0700

    [WIP] elliptic-curve: extract toplevel `Curve` trait
    This is a fairly substantial refactoring of the `elliptic-curve` crate
    which extracts a toplevel `elliptic_curve::Curve` trait and redefines
    all types to use it.
    
    The `elliptic_curve::weierstrass::Curve` trait is maintained, but
    relegated to a marker trait. That said, it's still used as the marker
    trait for all traits/types defined in the `elliptic_curve::weierstrass`
    module.
    
    Notably the main thing this facilitates is making `SecretKey` generic
    around a `C: elliptic_curve::Curve` generic type. The previous impetus
    was to avoid directly associating types dependent on scalars with
    specific elliptic curves (rather the curve's order), but this is
    ultimately unhelpful as it precludes accounting for trait impls on the
    curve in trait bounds.
    
    With the new approach, we can use trait bounds to conditionally define
    methods on `SecretKey`, e.g. bounding a `SecretKey::generate` impl on
    curves which impl `Arithmetic where Self::Scalar: Generate`. This would
    eliminate the need for a special `GenerateSecretKey` trait.
    
    One other notable change is the associated type for computing type sizes
    for a particular curve has been renamed to `ElementSize`, rather than
    the previous `ScalarSize`. This hopefully captures that this size is for
    all elements related to a particular curve, i.e. both the base and
    scalar fields.

--- elliptic-curve/src/weierstrass.rs
+++ elliptic-curve/src/weierstrass.rs
@@ -1,12 +1,12 @@
 //! Elliptic curves in short Weierstrass form.
 
-pub mod curve;
 pub mod point;
 pub mod public_key;
 
-pub use curve::Curve;
-pub use point::{CompressedPoint, CompressedPointSize, UncompressedPoint, UncompressedPointSize};
-pub use public_key::PublicKey;
+pub use self::{
+    point::{CompressedPoint, CompressedPointSize, UncompressedPoint, UncompressedPointSize},
+    public_key::PublicKey,
+};
 
 use crate::{consts::U1, ScalarBytes};
 use core::ops::Add;
@@ -19,21 +19,25 @@ use crate::secret_key::SecretKey;
 #[cfg(feature = "rand_core")]
 use rand_core::{CryptoRng, RngCore};
 
+/// Marker trait for elliptic curves in short Weierstrass form
+pub trait Curve: super::Curve {}
+
 /// Fixed-base scalar multiplication
 pub trait FixedBaseScalarMul: Curve
 where
-    <Self::ScalarSize as Add>::Output: Add<U1>,
-    CompressedPoint<Self>: From<Self::Point>,
-    UncompressedPoint<Self>: From<Self::Point>,
-    CompressedPointSize<Self::ScalarSize>: ArrayLength<u8>,
-    UncompressedPointSize<Self::ScalarSize>: ArrayLength<u8>,
+    Self::ElementSize: Add<U1>,
+    <Self::ElementSize as Add>::Output: Add<U1>,
+    CompressedPoint<Self>: From<Self::AffinePoint>,
+    UncompressedPoint<Self>: From<Self::AffinePoint>,
+    CompressedPointSize<Self>: ArrayLength<u8>,
+    UncompressedPointSize<Self>: ArrayLength<u8>,
 {
-    /// Elliptic curve point type
-    type Point: ConditionallySelectable;
+    /// Affine point type for this elliptic curve
+    type AffinePoint: ConditionallySelectable;
 
     /// Multiply the given scalar by the generator point for this elliptic
     /// curve.
-    fn mul_base(scalar: &ScalarBytes<Self::ScalarSize>) -> CtOption<Self::Point>;
+    fn mul_base(scalar: &ScalarBytes<Self>) -> CtOption<Self::AffinePoint>;
 }
 
 /// Generate a secret key for this elliptic curve
@@ -42,5 +46,5 @@ where
 pub trait GenerateSecretKey: Curve {
     /// Generate a random [`SecretKey`] for this elliptic curve using the
     /// provided [`CryptoRng`]
-    fn generate_secret_key(rng: impl CryptoRng + RngCore) -> SecretKey<Self::ScalarSize>;
+    fn generate_secret_key(rng: impl CryptoRng + RngCore) -> SecretKey<Self>;
 }
