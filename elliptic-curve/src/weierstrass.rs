920f20c603ab549f91e08b92dedb8dad0f900740
Tony Arcieri <bascule@gmail.com>
2020-08-04 13:51:21 -0700

    elliptic-curve: add `encoding` module
    Replaces the previous `FromPublicKey` and `FromSecretKey` traits with a
    more general `FromBytes` and `ToBytes` traits.
    
    These match the function signatures presently used in the existing
    crates, and are useful in cases where e.g. a scalar doesn't necessarily
    represent a secret (such as in ECDSA).

--- elliptic-curve/src/weierstrass.rs
+++ elliptic-curve/src/weierstrass.rs
@@ -5,7 +5,7 @@ pub mod public_key;
 
 pub use self::{
     point::{CompressedPoint, CompressedPointSize, UncompressedPoint, UncompressedPointSize},
-    public_key::{FromPublicKey, PublicKey},
+    public_key::PublicKey,
 };
 
 /// Marker trait for elliptic curves in short Weierstrass form
