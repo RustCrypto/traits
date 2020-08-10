b3f8d9e976bc4c3dd7d8a6766ad3772bcd872602
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2020-08-10 20:32:25 +0300

    Use SVG logo

--- elliptic-curve/src/lib.rs
+++ elliptic-curve/src/lib.rs
@@ -14,7 +14,8 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png",
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_root_url = "https://docs.rs/elliptic-curve/0.4.0"
 )]
 
