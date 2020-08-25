5dc014809413ee08129fdb29d3c89a3f8ac052f9
Tony Arcieri <bascule@gmail.com>
2020-08-25 09:36:00 -0700

    elliptic-curve: make weierstrass::point::Decode constant time
    Replaces boolean flag for y-coordinate even/odd with subtle::Choice

--- elliptic-curve/src/weierstrass/point.rs
+++ elliptic-curve/src/weierstrass/point.rs
@@ -2,11 +2,11 @@
 
 use super::Curve;
 use crate::ElementBytes;
-use subtle::CtOption;
+use subtle::{Choice, CtOption};
 
 /// Attempt to decompress an elliptic curve point from its x-coordinate and
 /// a boolean flag indicating whether or not the y-coordinate is odd.
 pub trait Decompress<C: Curve>: Sized {
     /// Attempt to decompress an elliptic curve point
-    fn decompress(x: &ElementBytes<C>, y_is_odd: bool) -> CtOption<Self>;
+    fn decompress(x: &ElementBytes<C>, y_is_odd: Choice) -> CtOption<Self>;
 }
