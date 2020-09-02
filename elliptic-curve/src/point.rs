bd1cc6c990d0603d15ab39811d3948ba094532d2
Tony Arcieri <bascule@gmail.com>
2020-09-02 09:13:51 -0700

    elliptic-curve: extract point::Compression trait
    Move point compression defaults onto their own trait

--- elliptic-curve/src/point.rs
+++ elliptic-curve/src/point.rs
@@ -1,5 +1,11 @@
 //! Traits for elliptic curve points
 
+/// Point compression settings
+pub trait Compression {
+    /// Should point compression be applied by default?
+    const COMPRESS_POINTS: bool;
+}
+
 /// Obtain the generator point.
 pub trait Generator {
     /// Get the generator point for this elliptic curve
--- elliptic-curve/src/weierstrass.rs
+++ elliptic-curve/src/weierstrass.rs
@@ -3,7 +3,4 @@
 pub mod point;
 
 /// Marker trait for elliptic curves in short Weierstrass form
-pub trait Curve: super::Curve {
-    /// Should point compression be applied by default?
-    const COMPRESS_POINTS: bool;
-}
+pub trait Curve: super::Curve {}
