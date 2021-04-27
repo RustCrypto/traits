08c3d733b51a86d5e9fbfee5e508474976066aba
Tony Arcieri <bascule@gmail.com>
2021-04-26 18:46:26 -0700

    aead: forbid `clippy::unwrap_used` (#616)

--- aead/src/lib.rs
+++ aead/src/lib.rs
@@ -15,7 +15,7 @@
 
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![forbid(unsafe_code)]
+#![forbid(unsafe_code, clippy::unwrap_used)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
