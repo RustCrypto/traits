d2639c11cbaa49db6186f99f1a0d90a44be146ad
Tony Arcieri <bascule@gmail.com>
2021-10-17 19:18:57 -0600

    elliptic-curve: bump `pkcs8`; update encoding traits (#789)
    Updates `pkcs8` and related crates to use the new trait names after they
    were renamed in RustCrypto/formats#121.

--- elliptic-curve/tests/pkcs8.rs
+++ elliptic-curve/tests/pkcs8.rs
@@ -7,7 +7,7 @@ use elliptic_curve::{
     sec1::ToEncodedPoint,
 };
 use hex_literal::hex;
-use pkcs8::{FromPrivateKey, FromPublicKey, PrivateKeyDocument, ToPrivateKey};
+use pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePrivateKey, PrivateKeyDocument};
 
 /// DER-encoded PKCS#8 public key
 const PKCS8_PUBLIC_KEY_DER: &[u8; 91] = include_bytes!("examples/pkcs8-public-key.der");
