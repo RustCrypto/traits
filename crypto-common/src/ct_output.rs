034d4f5494e211b06152c85fd5050e021ad4dbc4
Артём Павлов <newpavlov@gmail.com>
2021-10-20 18:33:10 +0300

    add inline attributes

--- crypto-common/src/ct_output.rs
+++ crypto-common/src/ct_output.rs
@@ -13,35 +13,41 @@ pub struct CtOutput<T: OutputSizeUser> {
 
 impl<T: OutputSizeUser> CtOutput<T> {
     /// Create a new [`CtOutput`] value.
+    #[inline(always)]
     pub fn new(bytes: Output<T>) -> Self {
         Self { bytes }
     }
 
     /// Get the inner [`Output`] array this type wraps.
+    #[inline(always)]
     pub fn into_bytes(self) -> Output<T> {
         self.bytes
     }
 }
 
 impl<T: OutputSizeUser> From<Output<T>> for CtOutput<T> {
+    #[inline(always)]
     fn from(bytes: Output<T>) -> Self {
         Self { bytes }
     }
 }
 
 impl<'a, T: OutputSizeUser> From<&'a Output<T>> for CtOutput<T> {
+    #[inline(always)]
     fn from(bytes: &'a Output<T>) -> Self {
         bytes.clone().into()
     }
 }
 
 impl<T: OutputSizeUser> ConstantTimeEq for CtOutput<T> {
+    #[inline(always)]
     fn ct_eq(&self, other: &Self) -> Choice {
         self.bytes.ct_eq(&other.bytes)
     }
 }
 
 impl<T: OutputSizeUser> PartialEq for CtOutput<T> {
+    #[inline(always)]
     fn eq(&self, x: &CtOutput<T>) -> bool {
         self.ct_eq(x).unwrap_u8() == 1
     }
