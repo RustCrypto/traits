086914c443aa55d4e253bbf551a7d760f8bb5305
Tony Arcieri <bascule@gmail.com>
2021-12-04 11:27:42 -0700

    elliptic-curve v0.11.5

--- elliptic-curve/src/lib.rs
+++ elliptic-curve/src/lib.rs
@@ -46,7 +46,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/elliptic-curve/0.11.4"
+    html_root_url = "https://docs.rs/elliptic-curve/0.11.5"
 )]
 
 #[cfg(feature = "alloc")]
