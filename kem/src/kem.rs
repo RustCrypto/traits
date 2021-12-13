4c80a8535499332f63a4e626f0e9006f257655f0
Michael Rosenberg <michael@mrosenberg.pub>
2021-12-13 16:23:24 -0500

    Initial commit of KEM traits

---
+++ kem/src/kem.rs
@@ -0,0 +1,64 @@
+//! KEM Traits
+
+use crate::errors::Error;
+use core::fmt::Debug;
+use generic_array::{ArrayLength, GenericArray};
+
+use rand_core::{CryptoRng, RngCore};
+
+/// Trait impl'd by concrete types that represent encapsulated keys. Besides encoding the behavior
+/// of the encapsulated key, this also requires
+pub trait EncappedKey: AsRef<[u8]> + Debug + Sized {
+    /// The size of the shared secret that this KEM produces
+    type NSecret: ArrayLength<u8>;
+
+    /// The public key type of this KEM. This is used for encapsulation and authenticated
+    /// decapsulation.
+    type PublicKey: AsRef<[u8]> + Debug + Sized;
+
+    /// Parse an encapsulated key from its byte representation
+    fn from_bytes(bytes: &[u8]) -> Result<Self, Error>;
+
+    /// Borrow a byte slice representing the serialized form of this
+    /// encapsulated key
+    fn as_bytes(&self) -> &[u8] {
+        self.as_ref()
+    }
+}
+
+/// Represents the functionality of a key encapsulator. For unauthenticated encapsulation, `Self`
+/// can be an empty struct. For authenticated encapsulation, `Self` is a private key.
+pub trait Encapsulator<EK: EncappedKey> {
+    /// Attempt to encapsulate a fresh shared secret with the given recipient. The resulting shared
+    /// secret is bound to the identity encoded in `Self` (i.e., authenticated wrt `Self`). If
+    /// `Self` is empty, then this is equivalent to unauthenticated encapsulation. Returns the
+    /// shared secret and encapsulated key on success, or an error if something went wrong.
+    fn try_encap<R>(
+        csprng: &mut R,
+        recip_pubkey: &EK::PublicKey,
+    ) -> Result<(EK, GenericArray<u8, EK::NSecret>), Error>
+    where
+        R: CryptoRng + RngCore;
+}
+
+/// Represents the functionality of a key decapsulator, where `Self` is a cryptographic key
+pub trait Decapsulator<EK: EncappedKey> {
+    /// Attempt to decapsulate the given encapsulated key. Returns the shared secret on success, or
+    /// an error if something went wrong.
+    fn try_decap(encapped_key: &EK) -> Result<GenericArray<u8, EK::NSecret>, Error>;
+}
+
+/// Represents the functionality of a authenticated-key decapsulator, where `Self` is a
+/// cryptographic key
+pub trait AuthDecapsulator<EK>
+where
+    EK: EncappedKey,
+{
+    /// Attempt to decapsulate the given encapsulated key. The resulting shared secret is bound to
+    /// the provided sender identity, thus providing authenticity. Returns the shared secret
+    /// success, or an error if something went wrong.
+    fn try_auth_decap(
+        encapped_key: &EK,
+        sender_pubkey: &EK::PublicKey,
+    ) -> Result<GenericArray<u8, EK::NSecret>, Error>;
+}
---
+++ kem/src/lib.rs
@@ -0,0 +1,72 @@
+//! RustCrypto: `kem` crate.
+//!
+//! Traits which provide generic, object-safe APIs for establishing shared secrets using a key
+//! encapsulation mechanism.
+//!
+//! ## Minimum Supported Rust Version
+//!
+//! Rust **1.56.0** or higher.
+//!
+//! Minimum supported Rust version may be changed in the future, but such changes will be
+//! accompanied with a minor version bump.
+//!
+//! ## SemVer policy
+//!
+//! - MSRV is considered exempt from SemVer as noted above
+//! - All on-by-default features of this library are covered by SemVer
+//!
+//! # Design
+//!
+//! This crate provides a common set of traits for non-interactively establishing secrets between
+//! peers. This is intended to be implemented by libraries which produce or contain implementations
+//! of key encapsulation mechanisms, and used by libraries which want to produce or consume
+//! encapsulated secrets while generically supporting any compatible backend.
+//!
+//! ## Goals
+//!
+//! The traits provided by this crate were designed with the following goals
+//! in mind:
+//!
+//! - Provide an easy-to-use, misuse resistant API optimized for consumers
+//!   (as opposed to implementers) of its traits.
+//! - Support common type-safe wrappers around "bag-of-bytes" representations
+//!   which can be directly parsed from or written to the "wire".
+//! - Allow one provider type to potentially implement support (including being generic over)
+//!   several key encapsulation mechanisms.
+//! - Keep KEM algorithm customizations / "knobs" out-of-band from the encap/decap APIs, ideally
+//!   pushing such concerns into the type system so that algorithm mismatches are caught as type
+//!   errors.
+//! - Opaque error type which minimizes information leaked from cryptographic failures
+//!
+//! ## Implementation
+//!
+//! To accomplish the above goals, the [`Encpsulator`], [`Decapsulator`], and [`AuthDecapsulator`]
+//! traits provided by this are generic over an [`EncapsulatedKey`] return value. Notably, they use
+//! such a parameter for the return value, allowing it to be inferred by the type checker based on
+//! the desired KEM algorithm.
+//!
+//! The [`Signature`] trait is requires `as_bytes()` and `from_bytes()` methods, enforcing that
+//! encapsulated key types are thin wrappers around a "bag-of-bytes" serialization. Inspiration for
+//! this approach comes from the Ed25519 signature system, which was based on the observation that
+//! past systems were not prescriptive about how signatures should be represented on-the-wire, and
+//! that lead to a proliferation of different wire formats and confusion about which ones should be
+//! used. This crate aims to provide similar simplicity by minimizing the number of steps involved
+//! to obtain a serializable encapsulated key.
+
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_root_url = "https://docs.rs/kem"
+)]
+#![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
+
+#[cfg(feature = "std")]
+extern crate std;
+
+mod errors;
+mod kem;
+
+pub use crate::{errors::*, kem::*};
