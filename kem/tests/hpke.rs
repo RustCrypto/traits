5f32283369bf1fe945aa6d7dd1c0485a5cde6af2
Michael Rosenberg <michael@mrosenberg.pub>
2021-12-15 14:04:00 -0500

    Removed unnecessary cfg(test)

--- kem/tests/hpke.rs
+++ kem/tests/hpke.rs
@@ -1,155 +1,145 @@
-#[cfg(test)]
-mod tests {
-    use generic_array::GenericArray;
-    use hpke::{
-        kem::{Kem as KemTrait, X25519HkdfSha256},
-        Deserializable as HpkeDeserializable, Serializable as HpkeSerializable,
-    };
-    use kem::{AuthDecapsulator, Decapsulator, EncappedKey, Encapsulator, Error};
-    use rand::rngs::OsRng;
-    use rand_core::{CryptoRng, RngCore};
+use generic_array::GenericArray;
+use hpke::{
+    kem::{Kem as KemTrait, X25519HkdfSha256},
+    Deserializable as HpkeDeserializable, Serializable as HpkeSerializable,
+};
+use kem::{AuthDecapsulator, Decapsulator, EncappedKey, Encapsulator, Error};
+use rand::rngs::OsRng;
+use rand_core::{CryptoRng, RngCore};
 
-    // Define the pubkey type. This has no trait bounds required by the library
-    #[derive(Clone)]
-    struct X25519PublicKey(<X25519HkdfSha256 as KemTrait>::PublicKey);
+// Define the pubkey type. This has no trait bounds required by the library
+#[derive(Clone)]
+struct X25519PublicKey(<X25519HkdfSha256 as KemTrait>::PublicKey);
 
-    // Define the encapsulated key type and impl the necessary traits. Since authenticated and
-    // unauthenticated DHKEMs have the same encapped key type, this will support both types of
-    // algorithms. In practice, one should use types to distinguish between the two. But this is
-    // just test code, so whatever.
-    #[derive(Debug)]
-    struct X25519EncappedKey(
-        // It's just an array of bytes
-        GenericArray<
-            u8,
-            <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeSerializable>::OutputSize,
-        >,
-    );
-    impl EncappedKey for X25519EncappedKey {
-        type NSecret = <X25519HkdfSha256 as KemTrait>::NSecret;
-        // In HPKE the only recipient public key is the identity key
-        type RecipPublicKey = X25519PublicKey;
-        // The sender's pubkey is the identity too
-        type SenderPublicKey = X25519PublicKey;
+// Define the encapsulated key type and impl the necessary traits. Since authenticated and
+// unauthenticated DHKEMs have the same encapped key type, this will support both types of
+// algorithms. In practice, one should use types to distinguish between the two. But this is just
+// test code, so whatever.
+#[derive(Debug)]
+struct X25519EncappedKey(
+    // It's just an array of bytes
+    GenericArray<u8, <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeSerializable>::OutputSize>,
+);
+impl EncappedKey for X25519EncappedKey {
+    type NSecret = <X25519HkdfSha256 as KemTrait>::NSecret;
+    // In HPKE the only recipient public key is the identity key
+    type RecipPublicKey = X25519PublicKey;
+    // The sender's pubkey is the identity too
+    type SenderPublicKey = X25519PublicKey;
+}
+impl AsRef<[u8]> for X25519EncappedKey {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
     }
-    impl AsRef<[u8]> for X25519EncappedKey {
-        fn as_ref(&self) -> &[u8] {
-            &self.0
-        }
-    }
+}
 
-    // Define some convenience types
-    type X25519PrivateKey = <X25519HkdfSha256 as KemTrait>::PrivateKey;
-    type SharedSecret = GenericArray<u8, <X25519EncappedKey as EncappedKey>::NSecret>;
+// Define some convenience types
+type X25519PrivateKey = <X25519HkdfSha256 as KemTrait>::PrivateKey;
+type SharedSecret = GenericArray<u8, <X25519EncappedKey as EncappedKey>::NSecret>;
 
-    // Define an authenticated encapsulator. To authenticate, we need a full sender keypair.
-    struct X25519AuthEncap(X25519PrivateKey, X25519PublicKey);
-    impl Encapsulator<X25519EncappedKey> for X25519AuthEncap {
-        fn try_encap<R: RngCore + CryptoRng>(
-            &self,
-            csprng: &mut R,
-            recip_pubkey: &X25519PublicKey,
-        ) -> Result<(X25519EncappedKey, SharedSecret), Error> {
-            <X25519HkdfSha256 as KemTrait>::encap(
-                &recip_pubkey.0,
-                Some((&self.0, &(self.1).0)),
-                csprng,
-            )
+// Define an authenticated encapsulator. To authenticate, we need a full sender keypair.
+struct X25519AuthEncap(X25519PrivateKey, X25519PublicKey);
+impl Encapsulator<X25519EncappedKey> for X25519AuthEncap {
+    fn try_encap<R: RngCore + CryptoRng>(
+        &self,
+        csprng: &mut R,
+        recip_pubkey: &X25519PublicKey,
+    ) -> Result<(X25519EncappedKey, SharedSecret), Error> {
+        <X25519HkdfSha256 as KemTrait>::encap(&recip_pubkey.0, Some((&self.0, &(self.1).0)), csprng)
             .map(|(ss, ek)| (X25519EncappedKey(ek.to_bytes()), ss.0))
             .map_err(|_| Error)
-        }
     }
+}
 
-    // Define an unauthenticated encapsulator. This doesn't need any state at all.
-    struct X25519Encap;
-    impl Encapsulator<X25519EncappedKey> for X25519Encap {
-        fn try_encap<R: RngCore + CryptoRng>(
-            &self,
-            csprng: &mut R,
-            recip_pubkey: &X25519PublicKey,
-        ) -> Result<(X25519EncappedKey, SharedSecret), Error> {
-            <X25519HkdfSha256 as KemTrait>::encap(&recip_pubkey.0, None, csprng)
-                .map(|(ss, ek)| (X25519EncappedKey(ek.to_bytes()), ss.0))
-                .map_err(|_| Error)
-        }
+// Define an unauthenticated encapsulator. This doesn't need any state at all.
+struct X25519Encap;
+impl Encapsulator<X25519EncappedKey> for X25519Encap {
+    fn try_encap<R: RngCore + CryptoRng>(
+        &self,
+        csprng: &mut R,
+        recip_pubkey: &X25519PublicKey,
+    ) -> Result<(X25519EncappedKey, SharedSecret), Error> {
+        <X25519HkdfSha256 as KemTrait>::encap(&recip_pubkey.0, None, csprng)
+            .map(|(ss, ek)| (X25519EncappedKey(ek.to_bytes()), ss.0))
+            .map_err(|_| Error)
     }
+}
 
-    // Define an decapsulator. Since authenticated and unauthenticated encapped keys are
-    // represented by the same type (which, outside of testing, should not be the case), this can
-    // do both auth'd and unauth'd decapsulation.
-    impl Decapsulator<X25519EncappedKey> for X25519PrivateKey {
-        fn try_decap(&self, encapped_key: &X25519EncappedKey) -> Result<SharedSecret, Error> {
-            // First parse the encapped key, since it's just bytes right now
-            let deserialized_encapped_key =
-                <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeDeserializable>::from_bytes(
-                    &encapped_key.0,
-                )
-                .map_err(|_| Error)?;
-
-            // Now decapsulate
-            <X25519HkdfSha256 as KemTrait>::decap(self, None, &deserialized_encapped_key)
-                .map(|ss| ss.0)
-                .map_err(|_| Error)
-        }
-    }
-    impl AuthDecapsulator<X25519EncappedKey> for X25519PrivateKey {
-        fn try_auth_decap(
-            &self,
-            encapped_key: &X25519EncappedKey,
-            sender_pubkey: &X25519PublicKey,
-        ) -> Result<SharedSecret, Error> {
-            // First parse the encapped key, since it's just bytes right now
-            let deserialized_encapped_key =
-                <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeDeserializable>::from_bytes(
-                    &encapped_key.0,
-                )
-                .map_err(|_| Error)?;
-
-            // Now decapsulate
-            <X25519HkdfSha256 as KemTrait>::decap(
-                self,
-                Some(&sender_pubkey.0),
-                &deserialized_encapped_key,
+// Define an decapsulator. Since authenticated and unauthenticated encapped keys are represented by
+// the same type (which, outside of testing, should not be the case), this can do both auth'd and
+// unauth'd decapsulation.
+impl Decapsulator<X25519EncappedKey> for X25519PrivateKey {
+    fn try_decap(&self, encapped_key: &X25519EncappedKey) -> Result<SharedSecret, Error> {
+        // First parse the encapped key, since it's just bytes right now
+        let deserialized_encapped_key =
+            <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeDeserializable>::from_bytes(
+                &encapped_key.0,
             )
+            .map_err(|_| Error)?;
+
+        // Now decapsulate
+        <X25519HkdfSha256 as KemTrait>::decap(self, None, &deserialized_encapped_key)
             .map(|ss| ss.0)
             .map_err(|_| Error)
-        }
     }
+}
+impl AuthDecapsulator<X25519EncappedKey> for X25519PrivateKey {
+    fn try_auth_decap(
+        &self,
+        encapped_key: &X25519EncappedKey,
+        sender_pubkey: &X25519PublicKey,
+    ) -> Result<SharedSecret, Error> {
+        // First parse the encapped key, since it's just bytes right now
+        let deserialized_encapped_key =
+            <<X25519HkdfSha256 as KemTrait>::EncappedKey as HpkeDeserializable>::from_bytes(
+                &encapped_key.0,
+            )
+            .map_err(|_| Error)?;
 
-    // A simple wrapper around the keypair generation function
-    fn gen_keypair<R: RngCore + CryptoRng>(csprng: &mut R) -> (X25519PrivateKey, X25519PublicKey) {
-        let (sk, pk) = X25519HkdfSha256::gen_keypair(csprng);
-        let wrapped_pk = X25519PublicKey(pk);
-
-        (sk, wrapped_pk)
+        // Now decapsulate
+        <X25519HkdfSha256 as KemTrait>::decap(
+            self,
+            Some(&sender_pubkey.0),
+            &deserialized_encapped_key,
+        )
+        .map(|ss| ss.0)
+        .map_err(|_| Error)
     }
+}
 
-    #[test]
-    fn test_hpke() {
-        let mut rng = OsRng;
+// A simple wrapper around the keypair generation function
+fn gen_keypair<R: RngCore + CryptoRng>(csprng: &mut R) -> (X25519PrivateKey, X25519PublicKey) {
+    let (sk, pk) = X25519HkdfSha256::gen_keypair(csprng);
+    let wrapped_pk = X25519PublicKey(pk);
 
-        // Make a sender and recipient keypair
-        let (sk_sender, pk_sender) = gen_keypair(&mut rng);
-        let (sk_recip, pk_recip) = gen_keypair(&mut rng);
+    (sk, wrapped_pk)
+}
 
-        // Try an unauthed encap first. Check that the derived shared secrets are equal
-        let encapper = X25519Encap;
-        let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
-        let ss2 = sk_recip.try_decap(&ek).unwrap();
-        assert_eq!(ss1, ss2);
+#[test]
+fn test_hpke() {
+    let mut rng = OsRng;
 
-        // Now do an authenticated encap
-        let encapper = X25519AuthEncap(sk_sender, pk_sender.clone());
-        let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
-        let ss2 = sk_recip.try_auth_decap(&ek, &pk_sender).unwrap();
-        assert_eq!(ss1, ss2);
+    // Make a sender and recipient keypair
+    let (sk_sender, pk_sender) = gen_keypair(&mut rng);
+    let (sk_recip, pk_recip) = gen_keypair(&mut rng);
 
-        // Now do an invalid authenticated encap, where the sender uses the wrong private key. This
-        // should produce unequal shared secrets.
-        let (rand_sk, _) = gen_keypair(&mut rng);
-        let encapper = X25519AuthEncap(rand_sk, pk_sender.clone());
-        let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
-        let ss2 = sk_recip.try_auth_decap(&ek, &pk_sender).unwrap();
-        assert_ne!(ss1, ss2);
-    }
+    // Try an unauthed encap first. Check that the derived shared secrets are equal
+    let encapper = X25519Encap;
+    let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
+    let ss2 = sk_recip.try_decap(&ek).unwrap();
+    assert_eq!(ss1, ss2);
+
+    // Now do an authenticated encap
+    let encapper = X25519AuthEncap(sk_sender, pk_sender.clone());
+    let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
+    let ss2 = sk_recip.try_auth_decap(&ek, &pk_sender).unwrap();
+    assert_eq!(ss1, ss2);
+
+    // Now do an invalid authenticated encap, where the sender uses the wrong private key. This
+    // should produce unequal shared secrets.
+    let (rand_sk, _) = gen_keypair(&mut rng);
+    let encapper = X25519AuthEncap(rand_sk, pk_sender.clone());
+    let (ek, ss1) = encapper.try_encap(&mut rng, &pk_recip).unwrap();
+    let ss2 = sk_recip.try_auth_decap(&ek, &pk_sender).unwrap();
+    assert_ne!(ss1, ss2);
 }
--- kem/tests/x3dh.rs
+++ kem/tests/x3dh.rs
@@ -1,144 +1,141 @@
-#[cfg(test)]
-mod tests {
-    use generic_array::{typenum::U32, GenericArray};
-    use kem::{AuthDecapsulator, EncappedKey, Encapsulator, Error};
-    use p256::ecdsa::Signature;
-    use rand::rngs::OsRng;
-    use rand_core::{CryptoRng, RngCore};
-    use x3dh_ke::{x3dh_a, x3dh_b, EphemeralKey, IdentityKey, Key, OneTimePreKey, SignedPreKey};
+use generic_array::{typenum::U32, GenericArray};
+use kem::{AuthDecapsulator, EncappedKey, Encapsulator, Error};
+use p256::ecdsa::Signature;
+use rand::rngs::OsRng;
+use rand_core::{CryptoRng, RngCore};
+use x3dh_ke::{x3dh_a, x3dh_b, EphemeralKey, IdentityKey, Key, OneTimePreKey, SignedPreKey};
 
-    // The size of an encapped key
-    const P256_POINT_SIZE: usize = 231;
+// The size of an encapped key
+const P256_POINT_SIZE: usize = 231;
 
-    // Define the sender pubkey type. This is an identity key;
-    type X3DhSenderPublicKey = IdentityKey;
-    // Define the recipient privkey type. This is a bundle of 3 privkeys of different lifespans
-    struct X3DhPrivkeyBundle {
-        ik: IdentityKey,
-        spk: SignedPreKey,
-        sig: Signature,
-        opk: OneTimePreKey,
+// Define the sender pubkey type. This is an identity key;
+type X3DhSenderPublicKey = IdentityKey;
+// Define the recipient privkey type. This is a bundle of 3 privkeys of different lifespans
+struct X3DhPrivkeyBundle {
+    ik: IdentityKey,
+    spk: SignedPreKey,
+    sig: Signature,
+    opk: OneTimePreKey,
+}
+impl X3DhPrivkeyBundle {
+    fn gen() -> X3DhPrivkeyBundle {
+        let ik = IdentityKey::default();
+        let spk = SignedPreKey::default();
+        let sig = ik.sign(&spk.pk_to_bytes());
+        let opk = OneTimePreKey::default();
+        X3DhPrivkeyBundle { ik, spk, sig, opk }
     }
-    impl X3DhPrivkeyBundle {
-        fn gen() -> X3DhPrivkeyBundle {
-            let ik = IdentityKey::default();
-            let spk = SignedPreKey::default();
-            let sig = ik.sign(&spk.pk_to_bytes());
-            let opk = OneTimePreKey::default();
-            X3DhPrivkeyBundle { ik, spk, sig, opk }
-        }
-        fn as_pubkeys(&self) -> X3DhPubkeyBundle {
-            X3DhPubkeyBundle {
-                ik: self.ik.strip(),
-                spk: self.spk.strip(),
-                opk: self.opk.strip(),
-                sig: self.sig,
-            }
+    fn as_pubkeys(&self) -> X3DhPubkeyBundle {
+        X3DhPubkeyBundle {
+            ik: self.ik.strip(),
+            spk: self.spk.strip(),
+            opk: self.opk.strip(),
+            sig: self.sig,
         }
     }
-    // The pubkeys keys associated with a privkey bundle. In x3dh-ke, all the keys serve as both
-    // pubkeys and privkeys. This seems dangerous but hey this isn't prod.
-    type X3DhPubkeyBundle = X3DhPrivkeyBundle;
+}
+// The pubkeys keys associated with a privkey bundle. In x3dh-ke, all the keys serve as both
+// pubkeys and privkeys. This seems dangerous but hey this isn't prod.
+type X3DhPubkeyBundle = X3DhPrivkeyBundle;
 
-    // The encapped key is just the byte repr of an ephemeral key. Impl the appropriate traits
-    #[derive(Debug)]
-    struct X3DhEncappedKey([u8; P256_POINT_SIZE]);
-    impl EncappedKey for X3DhEncappedKey {
-        type NSecret = U32;
-        type SenderPublicKey = X3DhSenderPublicKey;
-        type RecipPublicKey = X3DhPubkeyBundle;
+// The encapped key is just the byte repr of an ephemeral key. Impl the appropriate traits
+#[derive(Debug)]
+struct X3DhEncappedKey([u8; P256_POINT_SIZE]);
+impl EncappedKey for X3DhEncappedKey {
+    type NSecret = U32;
+    type SenderPublicKey = X3DhSenderPublicKey;
+    type RecipPublicKey = X3DhPubkeyBundle;
+}
+impl AsRef<[u8]> for X3DhEncappedKey {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
     }
-    impl AsRef<[u8]> for X3DhEncappedKey {
-        fn as_ref(&self) -> &[u8] {
-            &self.0
-        }
+}
+
+// The private key of an authenticated sender is just their identity key. Again, this is the same
+// type as the pubkey.
+type X3DhSenderPrivateKey = IdentityKey;
+type SharedSecret = GenericArray<u8, <X3DhEncappedKey as EncappedKey>::NSecret>;
+
+// Define an authenticated encapsulator. To authenticate, we need a full sender keypair.
+impl Encapsulator<X3DhEncappedKey> for X3DhSenderPrivateKey {
+    fn try_encap<R: RngCore + CryptoRng>(
+        &self,
+        _csprng: &mut R,
+        recip_pubkey: &X3DhPubkeyBundle,
+    ) -> Result<(X3DhEncappedKey, SharedSecret), Error> {
+        // Make a new ephemeral key. This will be the encapped key
+        let ek = EphemeralKey::default();
+        // Deconstruct the recipient's pubkey bundle
+        let X3DhPubkeyBundle { ik, spk, sig, opk } = recip_pubkey;
+
+        // Do the X3DH operation to get the shared secret
+        let shared_secret = x3dh_a(sig, self, spk, &ek, ik, opk)
+            .map(|ss| SharedSecret::clone_from_slice(&ss))
+            .map_err(|e| {
+                println!("err {:?}", e);
+                Error
+            })?;
+        // Serialize the ephemeral key
+        let encapped_key = {
+            let mut buf = [0u8; P256_POINT_SIZE];
+            buf.copy_from_slice(&ek.to_bytes());
+            X3DhEncappedKey(buf)
+        };
+
+        Ok((encapped_key, shared_secret))
     }
+}
 
-    // The private key of an authenticated sender is just their identity key. Again, this is the
-    // same type as the pubkey.
-    type X3DhSenderPrivateKey = IdentityKey;
-    type SharedSecret = GenericArray<u8, <X3DhEncappedKey as EncappedKey>::NSecret>;
+// Define an decapsulator. Since authenticated and unauthenticated encapped keys are represented by
+// the same type (which, outside of testing, should not be the case), this can do both auth'd and
+// unauth'd decapsulation.
+impl AuthDecapsulator<X3DhEncappedKey> for X3DhPrivkeyBundle {
+    fn try_auth_decap(
+        &self,
+        encapped_key: &X3DhEncappedKey,
+        sender_pubkey: &X3DhSenderPublicKey,
+    ) -> Result<SharedSecret, Error> {
+        // First parse the encapped key, since it's just bytes right now
+        let deserialized_ek = EphemeralKey::from_bytes(&encapped_key.0).map_err(|_| Error)?;
+        // Deconstruct our private keys bundle
+        let X3DhPubkeyBundle {
+            ik,
+            spk,
+            sig: _,
+            opk,
+        } = self;
 
-    // Define an authenticated encapsulator. To authenticate, we need a full sender keypair.
-    impl Encapsulator<X3DhEncappedKey> for X3DhSenderPrivateKey {
-        fn try_encap<R: RngCore + CryptoRng>(
-            &self,
-            _csprng: &mut R,
-            recip_pubkey: &X3DhPubkeyBundle,
-        ) -> Result<(X3DhEncappedKey, SharedSecret), Error> {
-            // Make a new ephemeral key. This will be the encapped key
-            let ek = EphemeralKey::default();
-            // Deconstruct the recipient's pubkey bundle
-            let X3DhPubkeyBundle { ik, spk, sig, opk } = recip_pubkey;
-
-            // Do the X3DH operation to get the shared secret
-            let shared_secret = x3dh_a(sig, self, spk, &ek, ik, opk)
-                .map(|ss| SharedSecret::clone_from_slice(&ss))
-                .map_err(|e| {
-                    println!("err {:?}", e);
-                    Error
-                })?;
-            // Serialize the ephemeral key
-            let encapped_key = {
-                let mut buf = [0u8; P256_POINT_SIZE];
-                buf.copy_from_slice(&ek.to_bytes());
-                X3DhEncappedKey(buf)
-            };
-
-            Ok((encapped_key, shared_secret))
-        }
+        // Now decapsulate
+        let buf = x3dh_b(sender_pubkey, spk, &deserialized_ek, ik, opk);
+        Ok(SharedSecret::clone_from_slice(&buf))
     }
+}
 
-    // Define an decapsulator. Since authenticated and unauthenticated encapped keys are
-    // represented by the same type (which, outside of testing, should not be the case), this can
-    // do both auth'd and unauth'd decapsulation.
-    impl AuthDecapsulator<X3DhEncappedKey> for X3DhPrivkeyBundle {
-        fn try_auth_decap(
-            &self,
-            encapped_key: &X3DhEncappedKey,
-            sender_pubkey: &X3DhSenderPublicKey,
-        ) -> Result<SharedSecret, Error> {
-            // First parse the encapped key, since it's just bytes right now
-            let deserialized_ek = EphemeralKey::from_bytes(&encapped_key.0).map_err(|_| Error)?;
-            // Deconstruct our private keys bundle
-            let X3DhPubkeyBundle {
-                ik,
-                spk,
-                sig: _,
-                opk,
-            } = self;
+#[test]
+fn test_x3dh() {
+    let mut rng = OsRng;
 
-            // Now decapsulate
-            let buf = x3dh_b(sender_pubkey, spk, &deserialized_ek, ik, opk);
-            Ok(SharedSecret::clone_from_slice(&buf))
-        }
-    }
+    // We use _a and _b suffixes to denote whether a key belongs to Alice or Bob. Alice is the
+    // sender in this case.
+    let sk_ident_a = X3DhSenderPrivateKey::default();
+    let pk_ident_a = sk_ident_a.strip();
+    let sk_bundle_b = X3DhPrivkeyBundle::gen();
+    let pk_bundle_b = sk_bundle_b.as_pubkeys();
 
-    #[test]
-    fn test_x3dh() {
-        let mut rng = OsRng;
+    // Now do an authenticated encap
+    let (encapped_key, ss1) = sk_ident_a.try_encap(&mut rng, &pk_bundle_b).unwrap();
+    let ss2 = sk_bundle_b
+        .try_auth_decap(&encapped_key, &pk_ident_a)
+        .unwrap();
+    assert_eq!(ss1, ss2);
 
-        // We use _a and _b suffixes to denote whether a key belongs to Alice or Bob. Alice is the
-        // sender in this case.
-        let sk_ident_a = X3DhSenderPrivateKey::default();
-        let pk_ident_a = sk_ident_a.strip();
-        let sk_bundle_b = X3DhPrivkeyBundle::gen();
-        let pk_bundle_b = sk_bundle_b.as_pubkeys();
-
-        // Now do an authenticated encap
-        let (encapped_key, ss1) = sk_ident_a.try_encap(&mut rng, &pk_bundle_b).unwrap();
-        let ss2 = sk_bundle_b
-            .try_auth_decap(&encapped_key, &pk_ident_a)
-            .unwrap();
-        assert_eq!(ss1, ss2);
-
-        // Now do an invalid authenticated encap, where the sender uses the wrong private key. This
-        // should produce unequal shared secrets.
-        let sk_ident_rando = X3DhSenderPrivateKey::default();
-        let (encapped_key, ss1) = sk_ident_rando.try_encap(&mut rng, &pk_bundle_b).unwrap();
-        let ss2 = sk_bundle_b
-            .try_auth_decap(&encapped_key, &pk_ident_a)
-            .unwrap();
-        assert_ne!(ss1, ss2);
-    }
+    // Now do an invalid authenticated encap, where the sender uses the wrong private key. This
+    // should produce unequal shared secrets.
+    let sk_ident_rando = X3DhSenderPrivateKey::default();
+    let (encapped_key, ss1) = sk_ident_rando.try_encap(&mut rng, &pk_bundle_b).unwrap();
+    let ss2 = sk_bundle_b
+        .try_auth_decap(&encapped_key, &pk_ident_a)
+        .unwrap();
+    assert_ne!(ss1, ss2);
 }
