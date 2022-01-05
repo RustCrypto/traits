688ca291aa0ab96f53f110e6b07adf44e529cc1c
Tony Arcieri <bascule@gmail.com>
2022-01-04 22:45:09 -0700

    async-signature v0.1.0

--- signature/async/src/lib.rs
+++ signature/async/src/lib.rs
@@ -21,7 +21,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/async-signature/0.0.1"
+    html_root_url = "https://docs.rs/async-signature/0.1.0"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
