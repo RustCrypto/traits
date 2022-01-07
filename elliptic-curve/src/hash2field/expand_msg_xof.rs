221474ee6dc2b9d05f04ac168e7391108bb34bd8
dAxpeDDa <daxpedda@gmail.com>
2022-01-07 06:25:58 +0100

    Enforce maximum size of `ExpandMsgXof`

--- elliptic-curve/src/hash2field/expand_msg_xof.rs
+++ elliptic-curve/src/hash2field/expand_msg_xof.rs
@@ -1,7 +1,8 @@
 use super::ExpandMsg;
 use crate::hash2field::Domain;
 use digest::{ExtendableOutput, ExtendableOutputDirty, Update, XofReader};
-use generic_array::{typenum::U32, ArrayLength};
+use generic_array::typenum::{IsLessOrEqual, U32, U65536};
+use generic_array::ArrayLength;
 
 /// Placeholder type for implementing expand_message_xof based on an extendable output function
 pub struct ExpandMsgXof<HashT>
@@ -15,13 +16,13 @@ where
 impl<HashT, L> ExpandMsg<L> for ExpandMsgXof<HashT>
 where
     HashT: Default + ExtendableOutput + ExtendableOutputDirty + Update,
-    L: ArrayLength<u8>,
+    L: ArrayLength<u8> + IsLessOrEqual<U65536>,
 {
     fn expand_message(msg: &[u8], dst: &'static [u8]) -> Self {
         let domain = Domain::<U32>::xof::<HashT>(dst);
         let reader = HashT::default()
             .chain(msg)
-            .chain([(L::to_usize() >> 8) as u8, L::to_usize() as u8])
+            .chain(L::to_u16().to_be_bytes())
             .chain(domain.data())
             .chain([domain.len() as u8])
             .finalize_xof();
