b13fc15664844418f74fc5f05173bd0d41e51eac
dAxpeDDa <daxpedda@gmail.com>
2022-01-07 08:33:24 +0100

    Remove unnecessary constrain

--- elliptic-curve/src/hash2field/expand_msg_xof.rs
+++ elliptic-curve/src/hash2field/expand_msg_xof.rs
@@ -1,14 +1,13 @@
 use super::ExpandMsg;
 use crate::hash2field::Domain;
 use digest::{ExtendableOutput, ExtendableOutputDirty, FixedOutput, Update, XofReader};
-use generic_array::typenum::{IsLessOrEqual, NonZero, U256, U32, U65536};
+use generic_array::typenum::{IsLessOrEqual, NonZero, U32, U65536};
 use generic_array::ArrayLength;
 
 /// Placeholder type for implementing expand_message_xof based on an extendable output function
 pub struct ExpandMsgXof<HashT>
 where
     HashT: Default + ExtendableOutput + ExtendableOutputDirty + FixedOutput + Update,
-    HashT::OutputSize: IsLessOrEqual<U256>,
 {
     reader: <HashT as ExtendableOutput>::Reader,
 }
@@ -18,10 +17,6 @@ impl<HashT, L> ExpandMsg<L> for ExpandMsgXof<HashT>
 where
     HashT: Default + ExtendableOutput + ExtendableOutputDirty + FixedOutput + Update,
     L: ArrayLength<u8>,
-    // If `len_in_bytes` is bigger then 256, length of the `DST` will depend on
-    // the output size of the hash, which is still not allowed to be bigger then 256:
-    // https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#section-5.4.2-5
-    HashT::OutputSize: IsLessOrEqual<U256>,
     // Constraint set by `expand_message_xof`:
     // https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#section-5.4.2-5
     L: NonZero + IsLessOrEqual<U65536>,
