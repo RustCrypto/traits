bf77a1194a0bb1c542a579e54febecb4cf6506f0
Tony Arcieri <bascule@gmail.com>
2022-01-19 15:17:46 -0700

    elliptic-curve v0.12.0-pre.1 (#916)

--- elliptic-curve/src/lib.rs
+++ elliptic-curve/src/lib.rs
@@ -4,7 +4,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/elliptic-curve/0.12.0-pre.0"
+    html_root_url = "https://docs.rs/elliptic-curve/0.12.0-pre.1"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
