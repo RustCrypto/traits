cbb5df92ec997c1fb4fe1caa28943abf5673a6e8
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2022-09-15 10:52:26 +0300

    signature: relax Keypair boundaries
    The Keypair trait might be useful for representing relationship between
    DigestSigner and DigestVerifier, RandomizedSigner and
    RandomizedVerifier, etc. in addition to just Signer/Verfifier pair.
    
    Relax type boundaries for the Keypair trait.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- signature/src/keypair.rs
+++ signature/src/keypair.rs
@@ -1,13 +1,13 @@
 //! Signing keypairs.
 
-use crate::{Signature, Signer, Verifier};
+use crate::Signature;
 
 /// Signing keypair with an associated verifying key.
 ///
 /// This represents a type which holds both a signing key and a verifying key.
-pub trait Keypair<S: Signature>: AsRef<Self::VerifyingKey> + Signer<S> {
+pub trait Keypair<S: Signature>: AsRef<Self::VerifyingKey> {
     /// Verifying key type for this keypair.
-    type VerifyingKey: Verifier<S>;
+    type VerifyingKey;
 
     /// Get the verifying key which can verify signatures produced by the
     /// signing key portion of this keypair.
