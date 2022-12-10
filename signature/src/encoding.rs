16ff629b20717e7ac3aacb9680ae80431ef5dbd1
Tony Arcieri <bascule@gmail.com>
2022-12-10 16:14:00 -0700

    signature: fallible encoding support
    This is helpful for "no panic" profiles.
    
    The `Error` type is deliberately left opaque, which means that
    `From`/`Into` still work via blanket `TryFrom`/`TryInto` impls with
    `Error = Infallible`.
    
    The `SignatureEncoding::to_bytes` method still provides infallible
    serialization.

--- signature/src/encoding.rs
+++ signature/src/encoding.rs
@@ -7,14 +7,17 @@ use alloc::vec::Vec;
 
 /// Support for decoding/encoding signatures as bytes.
 pub trait SignatureEncoding:
-    Clone + Sized + for<'a> TryFrom<&'a [u8], Error = Error> + Into<Self::Repr>
+    Clone + Sized + for<'a> TryFrom<&'a [u8], Error = Error> + TryInto<Self::Repr>
 {
     /// Byte representation of a signature.
     type Repr: 'static + AsRef<[u8]> + Clone + Send + Sync;
 
     /// Encode signature as its byte representation.
     fn to_bytes(&self) -> Self::Repr {
-        self.clone().into()
+        self.clone()
+            .try_into()
+            .ok()
+            .expect("signature encoding error")
     }
 
     /// Encode signature as a byte vector.
