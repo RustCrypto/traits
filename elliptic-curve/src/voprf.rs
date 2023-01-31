c0c47018b4754c43ee474ba5ea31de2b0b6c14a9
daxpedda <daxpedda@gmail.com>
2023-01-31 22:26:23 +0100

    Update VOPRF identifier type (#1175)

--- elliptic-curve/src/voprf.rs
+++ elliptic-curve/src/voprf.rs
@@ -7,14 +7,14 @@ use crate::PrimeCurve;
 /// Elliptic curve parameters used by VOPRF.
 pub trait VoprfParameters: PrimeCurve {
     /// The `ID` parameter which identifies a particular elliptic curve
-    /// as defined in [section 4 of `draft-irtf-cfrg-voprf-08`][voprf].
+    /// as defined in [section 4 of `draft-irtf-cfrg-voprf-19`][voprf].
     ///
-    /// [voprf]: https://www.ietf.org/archive/id/draft-irtf-cfrg-voprf-08.html#section-4
-    const ID: u16;
+    /// [voprf]: https://www.ietf.org/archive/id/draft-irtf-cfrg-voprf-19.html#name-ciphersuites-2
+    const ID: &'static str;
 
     /// The `Hash` parameter which assigns a particular hash function to this
-    /// ciphersuite as defined in [section 4 of `draft-irtf-cfrg-voprf-08`][voprf].
+    /// ciphersuite as defined in [section 4 of `draft-irtf-cfrg-voprf-19`][voprf].
     ///
-    /// [voprf]: https://www.ietf.org/archive/id/draft-irtf-cfrg-voprf-08.html#section-4
+    /// [voprf]: https://www.ietf.org/archive/id/draft-irtf-cfrg-voprf-19.html#name-ciphersuites-2
     type Hash: digest::Digest;
 }
