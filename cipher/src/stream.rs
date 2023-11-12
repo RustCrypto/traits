6e7ee07546ad42b9aba395ba15092199f0025545
Artyom Pavlov <newpavlov@gmail.com>
2023-11-12 19:23:52 +0300

    cipher: stream cipher improvements (#1388)

--- cipher/src/stream.rs
+++ cipher/src/stream.rs
@@ -202,21 +202,21 @@ macro_rules! impl_seek_num {
     {$($t:ty )*} => {
         $(
             impl SeekNum for $t {
-                fn from_block_byte<T: Counter>(block: T, byte: u8, bs: u8) -> Result<Self, OverflowError> {
-                    debug_assert!(byte < bs);
-                    let mut block: Self = block.try_into().map_err(|_| OverflowError)?;
-                    if byte != 0 {
-                        block -= 1;
-                    }
-                    let pos = block.checked_mul(bs as Self).ok_or(OverflowError)? + (byte as Self);
-                    Ok(pos)
+                fn from_block_byte<T: Counter>(block: T, byte: u8, block_size: u8) -> Result<Self, OverflowError> {
+                    debug_assert!(byte != 0);
+                    let rem = block_size.checked_sub(byte).ok_or(OverflowError)?;
+                    let block: Self = block.try_into().map_err(|_| OverflowError)?;
+                    block
+                        .checked_mul(block_size.into())
+                        .and_then(|v| v.checked_sub(rem.into()))
+                        .ok_or(OverflowError)
                 }
 
-                fn into_block_byte<T: Counter>(self, bs: u8) -> Result<(T, u8), OverflowError> {
-                    let bs = bs as Self;
-                    let byte = self % bs;
-                    let block = T::try_from(self/bs).map_err(|_| OverflowError)?;
-                    Ok((block, byte as u8))
+                fn into_block_byte<T: Counter>(self, block_size: u8) -> Result<(T, u8), OverflowError> {
+                    let bs: Self = block_size.into();
+                    let byte = (self % bs) as u8;
+                    let block = T::try_from(self / bs).map_err(|_| OverflowError)?;
+                    Ok((block, byte))
                 }
             }
         )*
