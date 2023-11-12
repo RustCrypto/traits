8f77112b2c07db000dfb7d7172abc9a7ed50d709
Tony Arcieri <bascule@gmail.com>
2023-11-11 22:41:07 -0700

    universal-hash: bump crypto-common to v0.2.0-pre; MSRV 1.65 (#1385)
    Replaces `generic-array` with `hybrid-array`, which is built on a
    combination of `typenum` and const generics, providing a degree of
    interoperability between the two systems.

--- universal-hash/src/lib.rs
+++ universal-hash/src/lib.rs
@@ -1,43 +1,24 @@
-//! Traits for [Universal Hash Functions].
-//!
-//! # About universal hashes
-//!
-//! Universal hash functions provide a "universal family" of possible
-//! hash functions where a given member of a family is selected by a key.
-//!
-//! They are well suited to the purpose of "one time authenticators" for a
-//! sequence of bytestring inputs, as their construction has a number of
-//! desirable properties such as pairwise independence as well as amenability
-//! to efficient implementations, particularly when implemented using SIMD
-//! instructions.
-//!
-//! When combined with a cipher, such as in Galois/Counter Mode (GCM) or the
-//! Salsa20 family AEAD constructions, they can provide the core functionality
-//! for a Message Authentication Code (MAC).
-//!
-//! [Universal Hash Functions]: https://en.wikipedia.org/wiki/Universal_hashing
-
 #![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
 )]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![deny(unsafe_code)]
+#![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
 
 #[cfg(feature = "std")]
 extern crate std;
 
 pub use crypto_common::{
-    self, generic_array,
+    self, array,
     typenum::{self, consts},
     Block, Key, KeyInit, ParBlocks, Reset,
 };
 
 use core::slice;
-use crypto_common::{BlockSizeUser, ParBlocksSizeUser};
-use generic_array::{ArrayLength, GenericArray};
+use crypto_common::{array::Array, BlockSizeUser, BlockSizes, ParBlocksSizeUser};
 use subtle::ConstantTimeEq;
 use typenum::Unsigned;
 
@@ -79,20 +60,20 @@ pub trait UniversalHash: BlockSizeUser + Sized {
     /// Update hash function state with the provided block.
     #[inline]
     fn update(&mut self, blocks: &[Block<Self>]) {
-        struct Ctx<'a, BS: ArrayLength<u8>> {
+        struct Ctx<'a, BS: BlockSizes> {
             blocks: &'a [Block<Self>],
         }
 
-        impl<'a, BS: ArrayLength<u8>> BlockSizeUser for Ctx<'a, BS> {
+        impl<'a, BS: BlockSizes> BlockSizeUser for Ctx<'a, BS> {
             type BlockSize = BS;
         }
 
-        impl<'a, BS: ArrayLength<u8>> UhfClosure for Ctx<'a, BS> {
+        impl<'a, BS: BlockSizes> UhfClosure for Ctx<'a, BS> {
             #[inline(always)]
             fn call<B: UhfBackend<BlockSize = BS>>(self, backend: &mut B) {
                 let pb = B::ParBlocksSize::USIZE;
                 if pb > 1 {
-                    let (par_blocks, tail) = to_blocks(self.blocks);
+                    let (par_blocks, tail) = array::slice_as_chunks(self.blocks);
                     for par_block in par_blocks {
                         backend.proc_par_blocks(par_block);
                     }
@@ -118,12 +99,12 @@ pub trait UniversalHash: BlockSizeUser + Sized {
     /// Message Authentication Codes (MACs) based on universal hashing.
     #[inline]
     fn update_padded(&mut self, data: &[u8]) {
-        let (blocks, tail) = to_blocks(data);
+        let (blocks, tail) = array::slice_as_chunks(data);
 
         self.update(blocks);
 
         if !tail.is_empty() {
-            let mut padded_block = GenericArray::default();
+            let mut padded_block = Array::default();
             padded_block[..tail.len()].copy_from_slice(tail);
             self.update(slice::from_ref(&padded_block));
         }
@@ -132,7 +113,7 @@ pub trait UniversalHash: BlockSizeUser + Sized {
     /// Retrieve result and consume hasher instance.
     fn finalize(self) -> Block<Self>;
 
-    /// Obtain the [`Output`] of a [`UniversalHash`] computation and reset it back
+    /// Obtain the output of a [`UniversalHash`] computation and reset it back
     /// to its initial state.
     #[inline]
     fn finalize_reset(&mut self) -> Block<Self>
@@ -173,17 +154,3 @@ impl core::fmt::Display for Error {
 
 #[cfg(feature = "std")]
 impl std::error::Error for Error {}
-
-/// Split message into slice of blocks and leftover tail.
-// TODO: replace with `slice::as_chunks` on migration to const generics
-#[inline(always)]
-fn to_blocks<T, N: ArrayLength<T>>(data: &[T]) -> (&[GenericArray<T, N>], &[T]) {
-    let nb = data.len() / N::USIZE;
-    let (left, right) = data.split_at(nb * N::USIZE);
-    let p = left.as_ptr() as *const GenericArray<T, N>;
-    // SAFETY: we guarantee that `blocks` does not point outside of `data`
-    // and `p` is valid for reads
-    #[allow(unsafe_code)]
-    let blocks = unsafe { slice::from_raw_parts(p, nb) };
-    (blocks, right)
-}
