017fa2972f23b9e67bcbff2ccca2f1905b8f4aa3
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-11-15 00:07:02 +0200

    as-ref suggestion

--- elliptic-curve/src/arithmetic.rs
+++ elliptic-curve/src/arithmetic.rs
@@ -88,7 +88,7 @@ pub trait PrimeCurveArithmetic:
 /// Normalize point(s) in projective representation by converting them to their affine ones.
 pub trait BatchNormalize<Points>: group::Curve {
     /// The output of the batch normalization; a container of affine points.
-    type Output: AsRef<Self::AffineRepr>;
+    type Output: AsRef<[Self::AffineRepr]>;
 
     /// Perform a batched conversion to affine representation on a sequence of projective points
     /// at an amortized cost that should be practically as efficient as a single conversion.
