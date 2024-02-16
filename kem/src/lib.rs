9ae0438e22d56fa4021bdde3d493f43f4f070898
Michael Rosenberg <michael@mrosenberg.pub>
2024-02-16 14:19:23 -0500

    Deleted cruft; Updated docs

--- kem/src/lib.rs
+++ kem/src/lib.rs
@@ -9,7 +9,24 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs, unused_qualifications, missing_debug_implementations)]
 
-mod kem;
+/// A value that can be encapsulated to. Often, this will just be a public key. However, it can
+/// also be a bundle of public keys, or it can include a sender's private key for authenticated
+/// encapsulation.
+pub trait Encapsulate<EK, SS> {
+    /// Encapsulation error
+    type Error;
 
-pub use crate::kem::*;
-pub use generic_array;
+    /// Encapsulates a fresh shared secret
+    fn encapsulate(&self, rng: impl rand_core::CryptoRngCore) -> Result<(EK, SS), Self::Error>;
+}
+
+/// A value that can be used to decapsulate an encapsulated key. Often, this will just be a secret
+/// key. But, as with [`Encapsulate`], it can be a bundle of secret keys, or it can include a
+/// sender's private key for authenticated encapsulation.
+pub trait Decapsulate<EK, SS> {
+    /// Decapsulation error
+    type Error;
+
+    /// Decapsulates the given encapsulated key
+    fn decapsulate(&self, encapsulated_key: &EK) -> Result<SS, Self::Error>;
+}
