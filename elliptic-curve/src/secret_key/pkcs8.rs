1b036fe61696772d74f5195db9f9338936795929
Arthur Gautier <baloo@superbaloo.net>
2024-08-19 17:26:24 +0000

    elliptic-curve: pkcs8 API changes
    see https://github.com/RustCrypto/formats/pull/1483

--- elliptic-curve/src/secret_key/pkcs8.rs
+++ elliptic-curve/src/secret_key/pkcs8.rs
@@ -16,7 +16,10 @@ use {
         sec1::{FromEncodedPoint, ToEncodedPoint},
         AffinePoint, CurveArithmetic,
     },
-    pkcs8::{der, EncodePrivateKey},
+    pkcs8::{
+        der::{self, asn1::OctetStringRef},
+        EncodePrivateKey,
+    },
 };
 
 // Imports for actual PEM support
@@ -39,19 +42,19 @@ where
     };
 }
 
-impl<C> TryFrom<pkcs8::PrivateKeyInfo<'_>> for SecretKey<C>
+impl<C> TryFrom<pkcs8::PrivateKeyInfoRef<'_>> for SecretKey<C>
 where
     C: AssociatedOid + Curve + ValidatePublicKey,
     FieldBytesSize<C>: ModulusSize,
 {
     type Error = pkcs8::Error;
 
-    fn try_from(private_key_info: pkcs8::PrivateKeyInfo<'_>) -> pkcs8::Result<Self> {
+    fn try_from(private_key_info: pkcs8::PrivateKeyInfoRef<'_>) -> pkcs8::Result<Self> {
         private_key_info
             .algorithm
             .assert_oids(ALGORITHM_OID, C::OID)?;
 
-        let ec_private_key = EcPrivateKey::from_der(private_key_info.private_key)?;
+        let ec_private_key = EcPrivateKey::from_der(private_key_info.private_key.as_bytes())?;
         Ok(Self::try_from(ec_private_key)?)
     }
 }
@@ -64,14 +67,16 @@ where
     FieldBytesSize<C>: ModulusSize,
 {
     fn to_pkcs8_der(&self) -> pkcs8::Result<der::SecretDocument> {
-        // TODO(tarcieri): make `PrivateKeyInfo` generic around `Params`
         let algorithm_identifier = pkcs8::AlgorithmIdentifierRef {
             oid: ALGORITHM_OID,
             parameters: Some((&C::OID).into()),
         };
 
         let ec_private_key = self.to_sec1_der()?;
-        let pkcs8_key = pkcs8::PrivateKeyInfo::new(algorithm_identifier, &ec_private_key);
+        let pkcs8_key = pkcs8::PrivateKeyInfoRef::new(
+            algorithm_identifier,
+            OctetStringRef::new(&ec_private_key)?,
+        );
         Ok(der::SecretDocument::encode_msg(&pkcs8_key)?)
     }
 }
