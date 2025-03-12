5006104d49de81aa3bf2deea9763fabca60cd1c8
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2025-03-12 17:05:21 -0400

    initial vrf crate structure

---
+++ vrf/src/lib.rs
@@ -0,0 +1,26 @@
+use digest::{Output, OutputSizeUser};
+
+pub trait Proof<H>
+where 
+    H: OutputSizeUser,
+{
+    fn to_hash(&self) -> Output<H>;
+}
+
+pub trait Prover<H>
+where
+    H: OutputSizeUser,
+{
+    type Proof: Proof<H>;
+    
+    fn prove(&self, alpha: &[u8]) -> Self::Proof;
+}
+
+pub trait Verifier<H>
+where
+    H: OutputSizeUser,
+{
+    type Proof: Proof<H>;
+    
+    fn verify(&self, alpha: &[u8], proof: Self::Proof) -> bool;
+}
