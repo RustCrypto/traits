d61cdbf62400879b6ba2ed28b292ea8bebdb6559
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-03-21 16:50:38 +0300

    aead: tweak `dev` module

--- aead/src/dev.rs
+++ aead/src/dev.rs
@@ -1,5 +1,80 @@
 //! Development-related functionality
 pub use blobby;
+use inout::InOutBuf;
+
+use crate::{Aead, AeadInOut, Nonce, Payload, Tag, TagPosition, array::typenum::Unsigned};
+
+/// Run AEAD test for the provided passing test vector
+#[allow(clippy::unwrap_used)]
+pub fn run_pass_test<C: AeadInOut>(
+    cipher: &C,
+    nonce: &Nonce<C>,
+    aad: &[u8],
+    pt: &[u8],
+    ct: &[u8],
+) -> Result<(), &'static str> {
+    let res = cipher
+        .encrypt(nonce, Payload { aad, msg: pt })
+        .map_err(|_| "encryption failure")?;
+    if res != ct {
+        return Err("encrypted data is different from target ciphertext");
+    }
+    let res = cipher
+        .decrypt(nonce, Payload { aad, msg: ct })
+        .map_err(|_| "decryption failure")?;
+    if res != pt {
+        return Err("decrypted data is different from target plaintext");
+    }
+
+    let (ct, tag) = match C::TAG_POSITION {
+        TagPosition::Prefix => {
+            let (tag, ct) = ct.split_at(C::TagSize::USIZE);
+            (ct, tag)
+        }
+        TagPosition::Postfix => ct.split_at(pt.len()),
+    };
+    let tag: &Tag<C> = tag.try_into().unwrap();
+
+    // Fill output buffer with "garbage" to test that its data does not get read during encryption
+    let mut buf: alloc::vec::Vec<u8> = (0..pt.len()).map(|i| i as u8).collect();
+    let inout_buf = InOutBuf::new(pt, &mut buf).unwrap();
+
+    let calc_tag = cipher
+        .encrypt_inout_detached(nonce, aad, inout_buf)
+        .map_err(|_| "encryption failure")?;
+    assert_eq!(tag, &calc_tag, "tag mismatch");
+    assert_eq!(ct, &buf, "ciphertext mismatch");
+
+    buf.iter_mut().enumerate().for_each(|(i, v)| *v = i as u8);
+
+    let inout_buf = InOutBuf::new(pt, &mut buf).unwrap();
+    cipher
+        .decrypt_inout_detached(nonce, aad, inout_buf, tag)
+        .map_err(|_| "decryption failure")?;
+    assert_eq!(ct, &buf, "plaintext mismatch");
+
+    Ok(())
+}
+
+/// Run AEAD test for the provided failing test vector
+pub fn run_fail_test<C: AeadInOut>(
+    cipher: &C,
+    nonce: &Nonce<C>,
+    aad: &[u8],
+    pt: &[u8],
+    ct: &[u8],
+) -> Result<(), &'static str> {
+    cipher
+        .encrypt(nonce, Payload { aad, msg: pt })
+        .map_err(|_| "encryption failure")?;
+
+    let res = cipher.decrypt(nonce, Payload { aad, msg: ct });
+    if res.is_ok() {
+        Err("decryption must return error")
+    } else {
+        Ok(())
+    }
+}
 
 /// Define AEAD test
 #[macro_export]
@@ -7,68 +82,32 @@ macro_rules! new_test {
     ($name:ident, $test_name:expr, $cipher:ty $(,)?) => {
         #[test]
         fn $name() {
-            use aead::{
-                Aead, KeyInit, Payload,
-                array::{Array, typenum::Unsigned},
-                dev::blobby::Blob6Iterator,
-            };
-
-            fn run_test(
-                key: &[u8],
-                nonce: &[u8],
-                aad: &[u8],
-                pt: &[u8],
-                ct: &[u8],
-                pass: bool,
-            ) -> Result<(), &'static str> {
-                let key = key.try_into().map_err(|_| "wrong key size")?;
-                let cipher = <$cipher>::new(key);
-                let nonce = nonce.try_into().map_err(|_| "wrong nonce size")?;
-
-                if !pass {
-                    let res = cipher.decrypt(nonce, Payload { aad: aad, msg: ct });
-                    if res.is_ok() {
-                        return Err("decryption must return error");
-                    }
-                    return Ok(());
-                }
-
-                let res = cipher
-                    .encrypt(nonce, Payload { aad: aad, msg: pt })
-                    .map_err(|_| "encryption failure")?;
-                if res != ct {
-                    return Err("encrypted data is different from target ciphertext");
-                }
-                let res = cipher
-                    .decrypt(nonce, Payload { aad: aad, msg: ct })
-                    .map_err(|_| "decryption failure")?;
-                if res != pt {
-                    return Err("decrypted data is different from target plaintext");
-                }
-                Ok(())
-            }
+            use $crate::dev::blobby::Blob6Iterator;
 
             let data = include_bytes!(concat!("data/", $test_name, ".blb"));
             for (i, row) in Blob6Iterator::new(data).unwrap().enumerate() {
                 let [key, nonce, aad, pt, ct, status] = row.unwrap();
-                let pass = match status[0] {
-                    0 => false,
-                    1 => true,
+                let key = key.try_into().map_err(|_| "wrong key size")?;
+                let nonce = nonce.try_into().map_err(|_| "wrong nonce size")?;
+                let cipher = $cipher::new(key);
+
+                let pass = match status {
+                    [0] => $crate::dev::run_fail_test(&cipher, nonce, aad, pt, ct),
+                    [1] => $crate::dev::run_pass_test(&cipher, nonce, aad, pt, ct),
                     _ => panic!("invalid value for pass flag"),
                 };
-                if let Err(reason) = run_test(key, nonce, aad, pt, ct, pass) {
+                let res = ;
+                if let Err(reason) = res {
                     panic!(
                         "\n\
-                            Failed test №{}\n\
-                            reason: \t{:?}\n\
-                            key:\t{:?}\n\
-                            nonce:\t{:?}\n\
-                            aad:\t{:?}\n\
-                            plaintext:\t{:?}\n\
-                            ciphertext:\t{:?}\n\
-                            pass:\t{}\n\
-                        ",
-                        i, reason, key, nonce, aad, pt, ct, pass,
+                        Failed test #{i}\n\
+                        reason:\t{reason:?}\n\
+                        key:\t{key:?}\n\
+                        nonce:\t{nonce:?}\n\
+                        aad:\t{aad:?}\n\
+                        plaintext:\t{pt:?}\n\
+                        ciphertext:\t{ct:?}\n\
+                        pass:\t{pass}\n"
                     );
                 }
             }
