6b9c5a9acc8229454a2f2e3f8fb9534d51cd42f9
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-04-07 16:29:09 +0300

    Move dummy hash to the tests folder

--- digest/src/newtype.rs
+++ digest/src/newtype.rs
@@ -11,7 +11,7 @@ macro_rules! newtype {
         pub struct $name($wrapped_ty);
 
         $(
-            newtype!(delegate_impl: $name($wrapped_ty) $trait_name);
+            $crate::newtype!(delegate_impl: $name($wrapped_ty) $trait_name);
         )*
 
         // Clone, Default, BlockSizeUser, CoreProxy, ExtendableOutput, FixedOutput, KeyInit,
@@ -204,65 +204,4 @@ macro_rules! newtype {
             }
         }
     };
-}
-
-#[cfg(test)]
-#[cfg(feature = "core-api")]
-mod tests {
-    use crate::HashMarker;
-    use crate::consts::U8;
-    use crate::core_api::{Buffer, BufferKindUser, CoreWrapper, FixedOutputCore, UpdateCore};
-    use crypto_common::{Block, Output, OutputSizeUser, Reset};
-
-    /// Core of primitive XOR hasher for testing purposes
-    #[derive(Clone, Default, Debug)]
-    pub struct FixedHashCore {
-        state: u64,
-    }
-
-    impl crate::core_api::BlockSizeUser for FixedHashCore {
-        type BlockSize = U8;
-    }
-
-    impl BufferKindUser for FixedHashCore {
-        type BufferKind = block_buffer::Eager;
-    }
-
-    impl Reset for FixedHashCore {
-        fn reset(&mut self) {
-            self.state = 0;
-        }
-    }
-
-    impl UpdateCore for FixedHashCore {
-        fn update_blocks(&mut self, blocks: &[Block<Self>]) {
-            for block in blocks {
-                self.state ^= u64::from_le_bytes(block.0)
-            }
-        }
-    }
-
-    impl HashMarker for FixedHashCore {}
-
-    impl OutputSizeUser for FixedHashCore {
-        type OutputSize = U8;
-    }
-
-    impl FixedOutputCore for FixedHashCore {
-        fn finalize_fixed_core(&mut self, buffer: &mut Buffer<Self>, out: &mut Output<Self>) {
-            let block = buffer.pad_with_zeros();
-            self.state ^= u64::from_le_bytes(block.0);
-            out.copy_from_slice(&self.state.to_le_bytes());
-        }
-    }
-
-    newtype!(
-        /// Primitive XOR hasher for testing purposes
-        FixedHash(CoreWrapper<FixedHashCore>);
-        delegate:
-            Debug AlgorithmName
-            Clone Default Reset
-            BlockSizeUser OutputSizeUser HashMarker
-            Update FixedOutput FixedOutputReset
-    );
 }
---
+++ digest/tests/dummy.rs
@@ -0,0 +1,60 @@
+#![cfg(feature = "core-api")]
+
+use digest::{
+    HashMarker,
+    consts::U8,
+    core_api::{Buffer, BufferKindUser, CoreWrapper, FixedOutputCore, UpdateCore},
+    crypto_common::{Block, BlockSizeUser, Output, OutputSizeUser, Reset},
+};
+
+/// Core of primitive XOR hasher for testing purposes
+#[derive(Clone, Default, Debug)]
+pub struct FixedHashCore {
+    state: u64,
+}
+
+impl BlockSizeUser for FixedHashCore {
+    type BlockSize = U8;
+}
+
+impl BufferKindUser for FixedHashCore {
+    type BufferKind = block_buffer::Eager;
+}
+
+impl Reset for FixedHashCore {
+    fn reset(&mut self) {
+        self.state = 0;
+    }
+}
+
+impl UpdateCore for FixedHashCore {
+    fn update_blocks(&mut self, blocks: &[Block<Self>]) {
+        for block in blocks {
+            self.state ^= u64::from_le_bytes(block.0)
+        }
+    }
+}
+
+impl HashMarker for FixedHashCore {}
+
+impl OutputSizeUser for FixedHashCore {
+    type OutputSize = U8;
+}
+
+impl FixedOutputCore for FixedHashCore {
+    fn finalize_fixed_core(&mut self, buffer: &mut Buffer<Self>, out: &mut Output<Self>) {
+        let block = buffer.pad_with_zeros();
+        self.state ^= u64::from_le_bytes(block.0);
+        out.copy_from_slice(&self.state.to_le_bytes());
+    }
+}
+
+digest::newtype!(
+    /// Primitive XOR hasher for testing purposes
+    FixedHash(CoreWrapper<FixedHashCore>);
+    delegate:
+        Debug AlgorithmName
+        Clone Default Reset
+        BlockSizeUser OutputSizeUser HashMarker
+        Update FixedOutput FixedOutputReset
+);
