ae58a7766614dab71973fb3286754f782ed90820
daxpedda <daxpedda@gmail.com>
2025-05-15 00:53:02 +0200

    Implement `PartialEq + Eq` for `NonIdentity` and `NonZeroScalar` (#1834)
    This is done by the fact that the underlying types implement these
    traits already.

--- elliptic-curve/src/point/non_identity.rs
+++ elliptic-curve/src/point/non_identity.rs
@@ -18,7 +18,7 @@ use crate::{CurveArithmetic, NonZeroScalar, Scalar};
 ///
 /// In the context of ECC, it's useful for ensuring that certain arithmetic
 /// cannot result in the identity point.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct NonIdentity<P> {
     point: P,
 }
