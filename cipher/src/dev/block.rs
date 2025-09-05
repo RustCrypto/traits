6809a414ab29578680d5e381bbc6ac1dbaa2ef8d
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-09-05 15:57:50 +0300

    cipher: tweak block cipher dev module

--- cipher/src/dev/block.rs
+++ cipher/src/dev/block.rs
@@ -13,38 +13,28 @@ pub struct TestVector {
     pub ciphertext: &'static [u8],
 }
 
-/// Block cipher encryption test
-pub fn block_cipher_enc_test<C: BlockCipherEncrypt + KeyInit>(
-    tv: &TestVector,
+fn encrypt_test_inner<C: BlockCipherEncrypt>(
+    cipher: &C,
+    pt: Block<C>,
+    ct: Block<C>,
 ) -> Result<(), &'static str> {
-    let Ok(state) = C::new_from_slice(tv.key) else {
-        return Err("cipher initialization failure");
-    };
-
-    let Ok(pt_block) = Block::<C>::try_from(tv.plaintext) else {
-        return Err("unexpected size of plaintext block");
-    };
-    let Ok(ct_block) = Block::<C>::try_from(tv.ciphertext) else {
-        return Err("unexpected size of ciphertext block");
-    };
-
-    let mut block = pt_block.clone();
-    state.encrypt_block(&mut block);
-    if block != ct_block {
+    let mut block = pt.clone();
+    cipher.encrypt_block(&mut block);
+    if block != ct {
         return Err("single block encryption failure");
     }
 
     let mut blocks1: [Block<C>; 101] = core::array::from_fn(|i| {
-        let mut block = pt_block.clone();
+        let mut block = pt.clone();
         block[0] ^= i as u8;
         block
     });
     let mut blocks2 = blocks1.clone();
 
     // Check that `encrypt_blocks` and `encrypt_block` result in the same ciphertext
-    state.encrypt_blocks(&mut blocks1);
+    cipher.encrypt_blocks(&mut blocks1);
     for b in blocks2.iter_mut() {
-        state.encrypt_block(b);
+        cipher.encrypt_block(b);
     }
     if blocks1 != blocks2 {
         return Err("multi-block encryption failure");
@@ -53,102 +43,121 @@ pub fn block_cipher_enc_test<C: BlockCipherEncrypt + KeyInit>(
     Ok(())
 }
 
-/// Block cipher encryption test
-pub fn block_cipher_dec_test<C: BlockCipherDecrypt + KeyInit>(
-    tv: &TestVector,
+fn decrypt_test_inner<C: BlockCipherDecrypt>(
+    cipher: &C,
+    pt: Block<C>,
+    ct: Block<C>,
 ) -> Result<(), &'static str> {
-    let Ok(state) = C::new_from_slice(tv.key) else {
-        return Err("cipher initialization failure");
-    };
-
-    let Ok(pt_block) = Block::<C>::try_from(tv.plaintext) else {
-        return Err("unexpected size of plaintext block");
-    };
-    let Ok(ct_block) = Block::<C>::try_from(tv.ciphertext) else {
-        return Err("unexpected size of ciphertext block");
-    };
-
-    let mut block = ct_block.clone();
-    state.decrypt_block(&mut block);
-    if block != pt_block {
+    let mut block = ct.clone();
+    cipher.decrypt_block(&mut block);
+    if block != pt {
         return Err("single block decryption failure");
     }
 
     let mut blocks1: [Block<C>; 101] = core::array::from_fn(|i| {
-        let mut block = ct_block.clone();
+        let mut block = ct.clone();
         block[0] ^= i as u8;
         block
     });
     let mut blocks2 = blocks1.clone();
 
     // Check that `encrypt_blocks` and `encrypt_block` result in the same ciphertext
-    state.decrypt_blocks(&mut blocks1);
+    cipher.decrypt_blocks(&mut blocks1);
     for b in blocks2.iter_mut() {
-        state.decrypt_block(b);
+        cipher.decrypt_block(b);
     }
     if blocks1 != blocks2 {
         return Err("multi-block decryption failure");
     }
-
     Ok(())
+}
+
+/// Block cipher encryption and decryption test
+pub fn encrypt_decrypt_test<C: BlockCipherEncrypt + BlockCipherDecrypt + KeyInit>(
+    tv: &TestVector,
+) -> Result<(), &'static str> {
+    let Ok(cipher) = C::new_from_slice(tv.key) else {
+        return Err("cipher initialization failure");
+    };
+
+    let Ok(pt) = Block::<C>::try_from(tv.plaintext) else {
+        return Err("unexpected size of plaintext block");
+    };
+    let Ok(ct) = Block::<C>::try_from(tv.ciphertext) else {
+        return Err("unexpected size of ciphertext block");
+    };
+
+    encrypt_test_inner(&cipher, pt.clone(), ct.clone())?;
+    decrypt_test_inner(&cipher, pt, ct)
+}
+
+/// Block cipher encryption test
+pub fn encrypt_test<C: BlockCipherEncrypt + KeyInit>(tv: &TestVector) -> Result<(), &'static str> {
+    let Ok(cipher) = C::new_from_slice(tv.key) else {
+        return Err("cipher initialization failure");
+    };
+
+    let Ok(pt) = Block::<C>::try_from(tv.plaintext) else {
+        return Err("unexpected size of plaintext block");
+    };
+    let Ok(ct) = Block::<C>::try_from(tv.ciphertext) else {
+        return Err("unexpected size of ciphertext block");
+    };
+
+    encrypt_test_inner(&cipher, pt, ct)
+}
+
+/// Block cipher encryption test
+pub fn decrypt_test<C: BlockCipherDecrypt + KeyInit>(tv: &TestVector) -> Result<(), &'static str> {
+    let Ok(cipher) = C::new_from_slice(tv.key) else {
+        return Err("cipher initialization failure");
+    };
+    let Ok(pt) = Block::<C>::try_from(tv.plaintext) else {
+        return Err("unexpected size of plaintext block");
+    };
+    let Ok(ct) = Block::<C>::try_from(tv.ciphertext) else {
+        return Err("unexpected size of ciphertext block");
+    };
+    decrypt_test_inner(&cipher, pt, ct)
 }
 
 /// Define block cipher test
 #[macro_export]
 macro_rules! block_cipher_test {
-    (
-        encdec: $name:ident, $test_name:expr, $cipher:ty $(,)?
-    ) => {
-        $crate::block_cipher_test!(
-            inner: $name, $test_name, $cipher,
-            "encryption" block_cipher_enc_test,
-            "decryption" block_cipher_dec_test,
-        );
+    // Encrypt/decrypt test with matching test and file names
+    ($name:ident, $cipher:ty $(,)?) => {
+        $crate::block_cipher_test!($name, stringify!($name), $cipher);
     };
-    (
-        enc: $name:ident, $test_name:expr, $cipher:ty $(,)?
-    ) => {
-        $crate::block_cipher_test!(
-            inner: $name, $test_name, $cipher,
-            "encryption" block_cipher_enc_test,
-        );
+    // Encrypt/decrypt test with custom test name
+    ($test_name:ident, $file_name:expr, $cipher:ty $(,)?) => {
+        $crate::block_cipher_test!($test_name, $file_name, $cipher, encrypt_decrypt_test);
     };
-    (
-        dec: $name:ident, $test_name:expr, $cipher:ty $(,)?
-    ) => {
-        $crate::block_cipher_test!(
-            inner: $name, $test_name, $cipher,
-            "decryption" block_cipher_dec_test,
-        );
+    // Test with custom test function and matching test and file names
+    ($name:ident, $cipher:ty, $test_fn:ident $(,)?) => {
+        $crate::block_cipher_test!($name, stringify!($name), $cipher, $test_fn);
     };
-
-    (
-        inner: $name:ident, $test_name:expr, $cipher:ty,
-        $($test_desc:literal $test_fn:ident,)*
-    ) => {
+    // Test with custom test function and test name
+    ($test_name:ident, $file_name:expr, $cipher:ty, $test_fn:ident $(,)?) => {
         #[test]
-        fn $name() {
+        fn $test_name() {
             use $crate::dev::block::TestVector;
 
             $crate::dev::blobby::parse_into_structs!(
-                include_bytes!(concat!("data/", $test_name, ".blb"));
+                include_bytes!(concat!("data/", $file_name, ".blb"));
                 static TEST_VECTORS: &[
                     TestVector { key, plaintext, ciphertext }
                 ];
             );
 
             for (i, tv) in TEST_VECTORS.iter().enumerate() {
-                $(
-                    let res = $test_fn(tv);
-                    if let Err(reason) = res {
-                        panic!(concat!(
-                            "\n\
-                            Failed ", $test_desc, " test #{i}\n\
-                            reason:\t{reason:?}\n\
-                            test vector:\t{tv:?}\n"
-                        ));
-                    }
-                )*
+                if let Err(reason) = $crate::dev::block::$test_fn::<$cipher>(tv) {
+                    panic!(
+                        "\n\
+                        Failed test #{i}\n\
+                        reason:\t{reason:?}\n\
+                        test vector:\t{tv:?}\n",
+                    );
+                }
             }
         }
     };
